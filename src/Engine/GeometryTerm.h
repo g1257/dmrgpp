968b027a6d277c146e2b22e464eaa4c1548530e4
g1257 <gz1@ornl.gov>
2010-07-19 20:33:40 -0400

    improving geometry/connectors/bonds module

---
+++ src/Engine/GeometryTerm.h
@@ -0,0 +1,115 @@
+// BEGIN LICENSE BLOCK
+/*
+Copyright © 2009 , UT-Battelle, LLC
+All rights reserved
+
+[DMRG++, Version 2.0.0]
+[by G.A., Oak Ridge National Laboratory]
+
+UT Battelle Open Source Software License 11242008
+
+OPEN SOURCE LICENSE
+
+Subject to the conditions of this License, each
+contributor to this software hereby grants, free of
+charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), a
+perpetual, worldwide, non-exclusive, no-charge,
+royalty-free, irrevocable copyright license to use, copy,
+modify, merge, publish, distribute, and/or sublicense
+copies of the Software.
+
+1. Redistributions of Software must retain the above
+copyright and license notices, this list of conditions,
+and the following disclaimer.  Changes or modifications
+to, or derivative works of, the Software should be noted
+with comments and the contributor and organization's
+name.
+
+2. Neither the names of UT-Battelle, LLC or the
+Department of Energy nor the names of the Software
+contributors may be used to endorse or promote products
+derived from this software without specific prior written
+permission of UT-Battelle.
+
+3. The software and the end-user documentation included
+with the redistribution, with or without modification,
+must include the following acknowledgment:
+
+"This product includes software produced by UT-Battelle,
+LLC under Contract No. DE-AC05-00OR22725  with the
+Department of Energy."
+ 
+*********************************************************
+DISCLAIMER
+
+THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER, CONTRIBUTORS, UNITED STATES GOVERNMENT,
+OR THE UNITED STATES DEPARTMENT OF ENERGY BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
+STATES DEPARTMENT OF ENERGY, NOR THE COPYRIGHT OWNER, NOR
+ANY OF THEIR EMPLOYEES, REPRESENTS THAT THE USE OF ANY
+INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+
+*********************************************************
+
+
+*/
+// END LICENSE BLOCK
+/** \ingroup DMRG */
+/*@{*/
+
+/*! \file GeometryTerm.h
+ *
+ * FIXME DOC
+ */
+#ifndef GEOMETRY_TERM_H
+#define GEOMETRY_TERM_H
+
+#include "Utils.h"
+
+namespace Dmrg {
+	
+	template<typename RealType>
+	class GeometryTerm {
+		public:
+			template<typename IoInputter>
+			void GeometryTerm(IoInputter& io,size_t i)
+			{
+				io.readline(x,"DegreesOfFreedom");
+				if (x<=0) throw std::runtime_error("DegreesOfFreedom<=0 is an error\n");
+				dof_ = x;
+				io.readline(s,"GeometryKind");
+				checkGeometry(s);
+				io.readline(s,"GeometryOptions");
+				checkOptions(s);
+				if (geometryKind_==LADDER || geometryKind_==LADDERX) {
+					io.readline(x,"LadderLeg");
+					if (x<2) throw std::runtime_error("LadderLeg<2 is an error\n");
+					leg_=x;
+				}
+				// here we read a vector of either numbers of matrices
+				// need to find a way to make this possible
+				io.readKnownSize();
+				
+			}
+	}; // class GeometryTerm
+} // namespace Dmrg 
+
+/*@}*/
+#endif // GEOMETRY_TERM_H
