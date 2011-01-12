c433d3d1f519f2298e5962d47ef23dc102a1e9bc
g1257 <gz1@ornl.gov>
2011-01-12 18:59:41 -0500

    Patch that allows to eliminate DMRG++'s dependency on the GSL

---
+++ src/Engine/DynamicTargettingEmpty.h
@@ -0,0 +1,261 @@
+
+/*
+Copyright (c) 2009, UT-Battelle, LLC
+All rights reserved
+
+[DMRG++, Version 2.0.0]
+[by G.A., Oak Ridge National Laboratory]
+
+UT Battelle Open Source Software License 11242008
+
+OPEN SOURCE LICENSE
+
+Subject to the conditions of this License, each
+contributor to this software hereby grants, free of
+charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), a
+perpetual, worldwide, non-exclusive, no-charge,
+royalty-free, irrevocable copyright license to use, copy,
+modify, merge, publish, distribute, and/or sublicense
+copies of the Software.
+
+1. Redistributions of Software must retain the above
+copyright and license notices, this list of conditions,
+and the following disclaimer.  Changes or modifications
+to, or derivative works of, the Software should be noted
+with comments and the contributor and organization's
+name.
+
+2. Neither the names of UT-Battelle, LLC or the
+Department of Energy nor the names of the Software
+contributors may be used to endorse or promote products
+derived from this software without specific prior written
+permission of UT-Battelle.
+
+3. The software and the end-user documentation included
+with the redistribution, with or without modification,
+must include the following acknowledgment:
+
+"This product includes software produced by UT-Battelle,
+LLC under Contract No. DE-AC05-00OR22725  with the
+Department of Energy."
+ 
+*********************************************************
+DISCLAIMER
+
+THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER, CONTRIBUTORS, UNITED STATES GOVERNMENT,
+OR THE UNITED STATES DEPARTMENT OF ENERGY BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
+STATES DEPARTMENT OF ENERGY, NOR THE COPYRIGHT OWNER, NOR
+ANY OF THEIR EMPLOYEES, REPRESENTS THAT THE USE OF ANY
+INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+
+*********************************************************
+*/
+
+#ifndef DYNAMICTARGETTING_H
+#define DYNAMICTARGETTING_H
+
+#include "ProgressIndicator.h"
+#include "BLAS.h"
+#include "ApplyOperatorLocal.h"
+#include "TimeSerializer.h"
+
+namespace Dmrg {
+	
+	template<
+		template<typename,typename,typename> class LanczosSolverTemplate,
+		template<typename,typename> class InternalProductTemplate,
+		typename WaveFunctionTransformationType_,
+		typename ModelType_,
+		typename ConcurrencyType_,
+		typename IoType_,
+		template<typename> class VectorWithOffsetTemplate>
+	class DynamicTargetting  {
+	public:
+		
+		typedef WaveFunctionTransformationType_ WaveFunctionTransformationType;
+		typedef ModelType_ ModelType;
+		typedef ConcurrencyType_ ConcurrencyType;
+		typedef IoType_ IoType;
+		typedef typename ModelType::RealType RealType;
+		typedef std::complex<RealType> ComplexType;
+		typedef InternalProductTemplate<ComplexType,ModelType> InternalProductType;
+		typedef typename ModelType::OperatorsType OperatorsType;
+		//typedef typename OperatorsType::SparseMatrixType SparseMatrixType;
+		typedef typename ModelType::MyBasisWithOperators BasisWithOperatorsType;
+		typedef std::vector<ComplexType> ComplexVectorType;
+		typedef LanczosSolverTemplate<RealType,InternalProductType,ComplexVectorType> LanczosSolverType;
+		//typedef std::vector<RealType> VectorType;
+		typedef psimag::Matrix<ComplexType> ComplexMatrixType;
+		//typedef typename LanczosSolverType::TridiagonalMatrixType TridiagonalMatrixType;
+		typedef typename BasisWithOperatorsType::OperatorType OperatorType;
+		typedef typename BasisWithOperatorsType::BasisType BasisType;
+		typedef TargetStructureParams<ModelType> TargettingStructureType; //3a-01
+		typedef typename BasisType::BlockType BlockType;
+		typedef VectorWithOffsetTemplate<ComplexType> VectorWithOffsetType;
+		typedef typename VectorWithOffsetType::VectorType VectorType;
+		typedef ComplexVectorType TargetVectorType;
+		typedef BlockMatrix<ComplexType,ComplexMatrixType> ComplexBlockMatrixType;
+		typedef ApplyOperatorLocal<BasisWithOperatorsType,VectorWithOffsetType,TargetVectorType> ApplyOperatorType;
+		typedef TimeSerializer<RealType,VectorWithOffsetType> TimeSerializerType;
+		
+		
+		enum {DISABLED,OPERATOR,WFT_NOADVANCE,WFT_ADVANCE};
+		enum {	EXPAND_ENVIRON=WaveFunctionTransformationType::EXPAND_ENVIRON,
+				EXPAND_SYSTEM=WaveFunctionTransformationType::EXPAND_SYSTEM,
+				INFINITE=WaveFunctionTransformationType::INFINITE};
+
+		static const size_t parallelRank_ = 0; // DYNT needs to support concurrency FIXME
+		
+		
+		DynamicTargetting(
+				const BasisWithOperatorsType& basisS,
+				const BasisWithOperatorsType& basisE,
+				const BasisType& basisSE,
+				const ModelType& model,
+				const TargettingStructureType& tstStruct,
+				const WaveFunctionTransformationType& wft)
+		:	
+		 	stage_(tstStruct.sites.size(),DISABLED),
+		 	basisS_(basisS),
+		 	basisE_(basisE),
+		 	basisSE_(basisSE),
+		 	model_(model),
+		 	tstStruct_(tstStruct),
+		 	waveFunctionTransformation_(wft),
+		 	progress_("DynamicTargetting",0),
+		 	currentOmega_(tstStruct_.omega),
+		 	targetVectors_(3),
+		 	weight_(targetVectors_.size()),
+		 	io_(tstStruct_.filename,parallelRank_),
+		 	applyOpLocal_(basisS,basisE,basisSE)
+		 	
+
+		{
+			if (!wft.isEnabled()) throw std::runtime_error(" DynamicTargetting "
+					"needs an enabled wft\n");
+			std::ostringstream msg;
+			msg<<"WARNING: Using a bogus DynamicTargetting module (DynamicDMRG will not work)\n";
+			progress_.printline(msg,std::cout);
+			bogus();
+		}
+		
+		
+		
+
+		RealType weight(size_t i) const
+		{
+			return 1.0;
+		}
+		
+
+		RealType gsWeight() const
+		{
+			return 1.0;  // bogus!!
+		}
+		
+
+		RealType normSquared(size_t i) const
+		{
+			// call to mult will conjugate one of the vector
+			return real(multiply(targetVectors_[i],targetVectors_[i]));
+		}
+		
+
+		template<typename SomeBasisType>
+		void setGs(const std::vector<TargetVectorType>& v,
+				const SomeBasisType& someBasis)
+		{
+			bogus();
+		}
+		
+
+		const ComplexType& operator[](size_t i) const { return psi_[i]; } // bogus!!
+					
+		ComplexType& operator[](size_t i) { return psi_[i]; } // bogus!!
+		
+
+		const VectorWithOffsetType& gs() const { return psi_; } // bogus!!
+		
+
+		bool includeGroundStage() const {return true; }  // bogus!!
+		
+
+		size_t size() const
+		{
+			return 0;  // bogus!!
+		}
+		
+
+		const VectorWithOffsetType& operator()(size_t i) const
+		{
+			return targetVectors_[i]; // bogus!!
+		}
+		
+
+		void evolve(RealType Eg,size_t direction,const BlockType& block,
+				size_t loopNumber, bool needsPrinting)
+		{
+			bogus();
+		}
+		
+
+		void initialGuess(VectorWithOffsetType& v) const
+		{
+			bogus();
+		}
+		
+
+		const BasisType& basisSE() const { return basisSE_; }
+
+		const BasisWithOperatorsType& basisS() const { return basisS_; }
+
+		const BasisWithOperatorsType& basisE() const { return basisE_; }
+		
+		
+	private:
+		
+		void bogus() const
+		{
+			throw std::runtime_error("This class is bogus!! You need to use"
+								" DynamicTargetting.h instead (which has GSL dependencies)!\n");
+		}
+		
+		std::vector<size_t> stage_;
+		VectorWithOffsetType psi_;
+		const BasisWithOperatorsType& basisS_;
+		const BasisWithOperatorsType& basisE_;
+		const BasisType& basisSE_;
+		const ModelType& model_;
+		const TargettingStructureType& tstStruct_;
+		const WaveFunctionTransformationType& waveFunctionTransformation_;
+		ProgressIndicator progress_;
+		RealType currentOmega_;
+		std::vector<VectorWithOffsetType> targetVectors_;
+		std::vector<RealType> weight_;
+		RealType gsWeight_;
+		typename IoType::Out io_;
+		ApplyOperatorType applyOpLocal_;
+		
+	}; // class DynamicTargettingEmpty
+	
+} // namespace
+#endif // DYNAMICTARGETTING_H
+
