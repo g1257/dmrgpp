b4ace270bfea1c2c7d00bcd1b44f5fd49eb6f09c
g1257 <gz1@ornl.gov>
2011-03-21 16:26:30 -0400

    cont. fraction driver brought up-to-date

--- drivers/continuedFraction.cpp
+++ drivers/continuedFraction.cpp
@@ -82,5 +82,10 @@ int main(int argc,char *argv[])
 
 	IoSimple::In io(file);
 	ContinuedFractionType cf(io);
-	cf.plot(wbegin,wend,wstep,delta);
+	typename ContinuedFractionType::PlotDataType v;
+	cf.plot(v,wbegin,wend,wstep,delta);
+	for (size_t x=0;x<v.size();x++) {
+		std::cout<<v[x].first<<" "<<std::real(v[x].second);
+		std::cout<<" "<<std::imag(v[x].second)<<"\n";
+	}
 }
