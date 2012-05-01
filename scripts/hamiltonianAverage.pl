12285276774229ffad846bad98a6db0d293e87fe
g1257 <gz1@ornl.gov>
2012-05-01 10:01:44 -0400

    Hamiltonian average script added

---
+++ scripts/hamiltonianAverage.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+my %val;
+my %seenTime;
+my $time = 0;
+
+while(<STDIN>) {
+	if (/Hamiltonian average at time=([^ ]+) for target=0 sector=[^ ]+ \<phi\(t\)\|H\|phi\(t\)\>=\(([^,]+),/) {
+		$time = $1;
+		if ($seenTime{$time}) {
+			$val{$time}=0;
+			$seenTime{$time}=0;
+		}
+		if (!defined($val{$time})) {
+			$val{$time} = $2;
+		} else {
+			$val{$time} += $2;
+		}
+	} else {
+		$seenTime{$time}=1;
+	}
+}
+
+foreach my $key (keys %val) {
+	print "$key $val{$key}\n";
+}
+
