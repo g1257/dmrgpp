b58e12d1889702f2b9ff836e5dd0c58ac7ecfae7
g1257 <gz1@ornl.gov>
2012-08-03 11:37:43 -0400

    bug fix in option 2 of triplet, tst does not always target gs now

--- src/Engine/Diagonalization.h
+++ src/Engine/Diagonalization.h
@@ -195,9 +195,10 @@ namespace Dmrg {
 			RealType gsEnergy = 0;
 			if (!target.includeGroundStage()) return gsEnergy;
 
-			bool onlyWft = ((parameters_.finiteLoop[loopIndex].
-					saveOption & 2)>0) ? true : false;
-			
+			bool onlyWft = false;
+			if (direction != WaveFunctionTransfType::INFINITE)
+				onlyWft = ((parameters_.finiteLoop[loopIndex].saveOption & 2)>0) ? true : false;
+		
 			std::ostringstream msg0;
 			msg0<<"Setting up Hamiltonian basis of size="<<lrs.super().size();
 			progress_.printline(msg0,std::cout);
