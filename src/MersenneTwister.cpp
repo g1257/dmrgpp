62e5a251824d81bf3cb64da6eaae2fabe4fb89dd
g1257 <gz1@ornl.gov>
2013-06-27 14:35:05 -0400

    added MersenneTwister

---
+++ src/MersenneTwister.cpp
@@ -0,0 +1,59 @@
+#include "MersenneTwister.h"
+
+namespace PsimagLite {
+
+MersenneTwister::MersenneTwister(unsigned s)
+    : index_(0)
+{
+	seed(s);
+}
+
+void
+MersenneTwister::seed(unsigned s)
+{
+	index_ = 0;
+	state_[0] = s;
+	for(unsigned i = 1; i < N_; ++i) {
+		state_[i] =  keepLast32BitMask_ &
+		        ( 1812433253 * ( state_[i-1]^( state_[i-1]>>30 ) + i ) );
+	}
+}
+
+void
+MersenneTwister::generate()
+{
+	for(unsigned i = 0; i < N_; ++i) {
+		unsigned y = (state_[i] & 0x80000000) +
+		        (state_[(i+1)%N_] & 0x7fffffff);
+		state_[i] = state_[(i+397)%N_] ^ (y >> 1);
+		if(y%2 != 0)
+			state_[i] ^= 2567483615;
+	}
+}
+
+unsigned
+MersenneTwister::random()
+{
+	if(index_ == 0)
+		generate();
+
+	unsigned y = state_[index_];
+	y ^= (y >> 11);
+	y ^= (y << 07) & 2636928640;
+	y ^= (y << 15) & 4022730752;
+	y ^= (y >> 18);
+
+	index_ = (index_+1)%N_;
+
+	return y;
+}
+
+double
+MersenneTwister::operator() ()
+{
+	unsigned r = random();
+	return (static_cast<double>(r)/keepLast32BitMask_);
+}
+
+} // namespace PsimagLite
+
---
+++ src/MersenneTwister.h
@@ -0,0 +1,47 @@
+#ifndef MERSENNETWISTER_H
+#define MERSENNETWISTER_H
+
+namespace PsimagLite {
+
+class MersenneTwister {
+
+public:
+
+	MersenneTwister(unsigned seed);
+
+	void seed(unsigned s);
+
+	unsigned random();
+
+	double operator() ();
+
+	static unsigned max() { return keepLast32BitMask_; }
+
+private:
+
+	static const unsigned N_ = 624;
+	static const unsigned keepLast32BitMask_ = 4294967295; // 2^32 - 1
+	unsigned index_;
+	unsigned state_[N_];
+	void generate();
+};
+
+} // namespace PsimagLite
+
+/*  K. A. Al-Hassanieh
+ *
+ *  MersenneTwister generates random 32-bit unsinged integers.
+ *  The constructor takes an unsigned int as a seed.
+ *  The return type of random() is unsigned int. Converting the
+ *  output to int can cause errors, since the returned value can
+ *  be larger than the max of signed int  which is 2^16 - 1:
+ *  int i = rng.random(); // causes errors
+ *  instead use:
+ *  unsigned i = rng.random();
+ *  or
+ *  size_t i = rng.random();
+ *
+ *  operator() returns a double between 0 and 1.
+ */
+
+#endif // MERSENNETWISTER_H
