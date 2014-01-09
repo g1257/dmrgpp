7af94911640be04e36cd883af147536d96bad14f
g1257 <gz1@ornl.gov>
2014-01-09 17:31:40 -0500

    parameter MatrixVectorStored defaults to zero

--- src/Engine/ParametersDmrgSolver.h
+++ src/Engine/ParametersDmrgSolver.h
@@ -308,7 +308,7 @@ namespace Dmrg {
 		      lanczosEps(1e-12),
 		      sitesPerBlock(1),
 		      gsWeight(false,0.0),
-		      maxMatrixRankStored(lanczosSteps)
+		      maxMatrixRankStored(0)
 		{
 			io.readline(model,"Model=");
 			io.readline(options,"SolverOptions=");
@@ -459,8 +459,6 @@ namespace Dmrg {
 			try {
 				io.readline(maxMatrixRankStored,"MaxMatrixRankStored=");
 			} catch (std::exception& e) {}
-
-
 		}
 	};
 
