90d74440d6955f78cb45d3939398830d4b43e752
g1257 <gz1@ornl.gov>
2014-01-29 17:06:17 -0500

    added TargetingBase

---
+++ src/Engine/TargetingBase.h
@@ -0,0 +1,233 @@
+/*
+Copyright (c) 2014, UT-Battelle, LLC
+All rights reserved
+
+[DMRG++, Version 3.0]
+[by G.A., Oak Ridge National Laboratory]
+
+UT Battelle Open Source Software License 11242008
+
+OPEN SOURCE LICENSE
+
+Subject to the conditions of this License, each
+contributor to this software hereby grants, free of
+charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), a
+perpetual, worldwide, non-exclusive, no-charge,
+royalty-free, irrevocable copyright license to use, copy,
+modify, merge, publish, distribute, and/or sublicense
+copies of the Software.
+
+1. Redistributions of Software must retain the above
+copyright and license notices, this list of conditions,
+and the following disclaimer.  Changes or modifications
+to, or derivative works of, the Software should be noted
+with comments and the contributor and organization's
+name.
+
+2. Neither the names of UT-Battelle, LLC or the
+Department of Energy nor the names of the Software
+contributors may be used to endorse or promote products
+derived from this software without specific prior written
+permission of UT-Battelle.
+
+3. The software and the end-user documentation included
+with the redistribution, with or without modification,
+must include the following acknowledgment:
+
+"This product includes software produced by UT-Battelle,
+LLC under Contract No. DE-AC05-00OR22725  with the
+Department of Energy."
+
+*********************************************************
+DISCLAIMER
+
+THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER, CONTRIBUTORS, UNITED STATES GOVERNMENT,
+OR THE UNITED STATES DEPARTMENT OF ENERGY BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
+STATES DEPARTMENT OF ENERGY, NOR THE COPYRIGHT OWNER, NOR
+ANY OF THEIR EMPLOYEES, REPRESENTS THAT THE USE OF ANY
+INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+
+*********************************************************
+
+*/
+
+/** \ingroup DMRG */
+/*@{*/
+
+/*! \file TargetingBase.h
+ *
+ *
+ *
+ */
+
+#ifndef TARGETING_BASE_H
+#define TARGETING_BASE_H
+#include <iostream>
+#include "String.h"
+#include "TargetParamsBase.h"
+#include "TargetHelper.h"
+#include "TargetingCommon.h"
+
+namespace Dmrg {
+
+template<template<typename,typename,typename> class LanczosSolverTemplate,
+         typename MatrixVectorType_,
+         typename WaveFunctionTransfType_,
+         typename IoType_>
+class TargetingBase {
+
+public:
+
+	typedef MatrixVectorType_ MatrixVectorType;
+	typedef typename MatrixVectorType::ModelType ModelType;
+	typedef IoType_ IoType;
+	typedef PsimagLite::IoSimple::In IoInputType;
+	typedef typename ModelType::RealType RealType;
+	typedef PsimagLite::ParametersForSolver<RealType> ParametersForSolverType;
+	typedef typename ModelType::ModelHelperType ModelHelperType;
+	typedef typename ModelHelperType::LeftRightSuperType LeftRightSuperType;
+	typedef typename LeftRightSuperType::BasisWithOperatorsType BasisWithOperatorsType;
+	typedef typename BasisWithOperatorsType::SparseMatrixType SparseMatrixType;
+	typedef typename BasisWithOperatorsType::OperatorType OperatorType;
+	typedef typename BasisWithOperatorsType::BasisType BasisType;
+	typedef typename SparseMatrixType::value_type ComplexOrRealType;
+	typedef typename PsimagLite::Vector<ComplexOrRealType>::Type VectorType;
+	typedef LanczosSolverTemplate<ParametersForSolverType,
+	                              MatrixVectorType,
+	                              VectorType> LanczosSolverType;
+	typedef typename BasisType::BlockType BlockType;
+	typedef WaveFunctionTransfType_ WaveFunctionTransfType;
+	typedef typename WaveFunctionTransfType::VectorWithOffsetType VectorWithOffsetType;
+	typedef TargetParamsBase<ModelType> TargettingParamsType;
+	typedef TargetHelper<ModelType,
+	                     TargettingParamsType,
+	                     WaveFunctionTransfType> TargetHelperType;
+	typedef TargetingCommon<TargetHelperType,
+	                        VectorWithOffsetType,
+	                        LanczosSolverType> TargetingCommonType;
+	typedef typename TargetingCommonType::ApplyOperatorExpressionType ApplyOperatorExpressionType;
+
+	enum {DISABLED=ApplyOperatorExpressionType::DISABLED,
+		  OPERATOR=ApplyOperatorExpressionType::OPERATOR,
+		  WFT_NOADVANCE=ApplyOperatorExpressionType::WFT_NOADVANCE};
+
+	TargetingBase(const LeftRightSuperType& lrs,
+	              const ModelType& model,
+	              const TargettingParamsType& tstStruct,
+	              const WaveFunctionTransfType& wft,
+	              SizeType targets,
+	              SizeType indexNoAdvance)
+	    : lrs_(lrs),
+	      model_(model),
+	      waveFunctionTransformation_(wft),
+	      commonTargetting_(lrs,model,tstStruct,wft,targets,indexNoAdvance)
+	{}
+
+	virtual RealType gsWeight() const = 0;
+
+	virtual void evolve(RealType Eg,
+	                    SizeType direction,
+	                    const BlockType& block1,
+	                    const BlockType& block2,
+	                    SizeType loopNumber) = 0;
+
+	SizeType size() const
+	{
+		if (commonTargetting_.allStages(DISABLED)) return 0;
+		return commonTargetting_.targetVectors().size();
+	}
+
+	const ModelType& model() const { return model_; }
+
+	RealType normSquared(SizeType i) const
+	{
+		throw PsimagLite::RuntimeError("TargetingBase: What are you doing here?\n");
+	}
+
+	RealType weight(SizeType i) const
+	{
+		throw PsimagLite::RuntimeError("TargetingBase: What are you doing here?\n");
+		return 0;
+	}
+
+	template<typename SomeBasisType>
+	void setGs(const typename PsimagLite::Vector<VectorType>::Type& v,
+	           const SomeBasisType& someBasis)
+	{
+		commonTargetting_.setGs(v,someBasis);
+	}
+
+	const VectorWithOffsetType& gs() const
+	{
+		return commonTargetting_.psi();
+	}
+
+	bool includeGroundStage() const {return true; }
+
+	const VectorWithOffsetType& operator()(SizeType i) const
+	{
+		throw PsimagLite::RuntimeError("TargetingBase::operator()(...)\n");
+	}
+
+	const LeftRightSuperType& leftRightSuper() const
+	{
+		return lrs_;
+	}
+
+	void initialGuess(VectorWithOffsetType& initialVector,
+	                  const typename PsimagLite::Vector<SizeType>::Type& block) const
+	{
+		commonTargetting_.initialGuess(initialVector,block);
+	}
+
+	RealType time() const { return 0; }
+
+	void updateOnSiteForTimeDep(BasisWithOperatorsType& basisWithOps) const
+	{
+		// nothing to do here
+	}
+
+	bool end() const { return false; }
+
+protected:
+
+	TargetingCommonType& common()
+	{
+		return commonTargetting_;
+	}
+
+	const TargetingCommonType& common() const
+	{
+		return commonTargetting_;
+	}
+
+private:
+
+	const LeftRightSuperType& lrs_;
+	const ModelType& model_;
+	const WaveFunctionTransfType& waveFunctionTransformation_;
+	TargetingCommonType commonTargetting_;
+};     //class TargetingBase
+
+} // namespace Dmrg
+/*@}*/
+#endif
+
