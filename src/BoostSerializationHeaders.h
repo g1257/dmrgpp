3e5cf4347881c0a69c0f7f22b683543d82b8c5f3
g1257 <gz1@ornl.gov>
2014-06-17 14:21:13 -0400

    BoostSerializationHeaders

---
+++ src/BoostSerializationHeaders.h
@@ -0,0 +1,12 @@
+#ifndef PSI_BOOST_SER_H_H
+#define PSI_BOOST_SER_H_H
+
+#ifdef USE_BOOST
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/vector.hpp>
+#endif
+
+#endif
+
