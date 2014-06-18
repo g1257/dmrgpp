2d2d1e0917f8296781222e77c732d41776722bc7
g1257 <gz1@ornl.gov>
2014-06-18 12:46:49 -0400

    fallback for boost serialization

--- src/BoostSerializationHeaders.h
+++ src/BoostSerializationHeaders.h
@@ -6,6 +6,22 @@
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/utility.hpp>
 #include <boost/serialization/vector.hpp>
+#else
+namespace boost {
+
+namespace serialization {
+
+template<typename T>
+class base_object {
+};
+
+} // namespace serialization
+class archive {
+
+        class text_iarchive {};
+};
+
+} // namespace boost
 #endif
 
 #endif
