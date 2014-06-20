05a5545d7dd5b5e2cc739bc0c35b9560a5db64e9
g1257 <gz1@ornl.gov>
2014-06-20 14:16:30 -0400

    fallback for boost serialization (again)

--- src/BoostSerializationHeaders.h
+++ src/BoostSerializationHeaders.h
@@ -1,5 +1,6 @@
 #ifndef PSI_BOOST_SER_H_H
 #define PSI_BOOST_SER_H_H
+#include <exception>
 
 #ifdef USE_BOOST
 #include <boost/archive/text_oarchive.hpp>
@@ -9,6 +10,13 @@
 #else
 namespace boost {
 
+void noboost(std::string msg)
+{
+	std::string str("ERROR: Please compile with -DUSE_BOOST for ");
+	str += msg + " to work\n";
+	throw std::runtime_error(str);
+}
+
 namespace serialization {
 
 template<typename T>
@@ -16,10 +24,45 @@ class base_object {
 };
 
 } // namespace serialization
-class archive {
 
-        class text_iarchive {};
+namespace archive {
+
+class text_iarchive {
+
+public:
+
+	text_iarchive(std::ifstream&)
+	{
+		boost::noboost("text_oarchive");
+	}
 };
+
+template<typename T>
+text_iarchive& operator>>(text_iarchive& ia, const T& t)
+{
+	boost::noboost("operator>> text_iarchive");
+	return ia;
+}
+
+class text_oarchive {
+
+public:
+
+	text_oarchive(std::ofstream&)
+	{
+		boost::noboost("text_oarchive");
+	}
+
+};
+
+template<typename T>
+text_oarchive& operator<<(text_oarchive& oa, const T& t)
+{
+	boost::noboost("operator<< text_oarchive");
+	return oa;
+}
+
+} // namespace archive
 
 } // namespace boost
 #endif
