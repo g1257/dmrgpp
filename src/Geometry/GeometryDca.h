c8fed311449632c2499573ab648eb4cff6b7135e
g1257 <gz1@ornl.gov>
2014-10-01 19:10:34 -0400

    GeometryDca checks if it is enabled

--- src/Geometry/GeometryDca.h
+++ src/Geometry/GeometryDca.h
@@ -31,27 +31,40 @@ public:
 	typedef RealType_ RealType;
 
 	GeometryDca(const GeometryType& g, SizeType orbitals)
+	    : enabled_(false),
+	      message_(g.label(0) + " with " + ttos(orbitals))
 	{
-		if (g.label(0) == "star" && orbitals == 4) return;
-
-		PsimagLite::String str("GeometryDca:: Only valid for 2x2 clusters\n");
-		str += "Not for " + g.label(0) + " with " + ttos(orbitals) + " orbitals.\n";
-		throw PsimagLite::RuntimeError(str);
+		if (g.label(0) == "star" && orbitals == 4)
+			enabled_=true;
 	}
 
 	SizeType kSum(SizeType k1, SizeType k2) const
 	{
+		if (!enabled_) printErrorAndDie("kSum");
 		return (k1^k2);
 	}
 
 	SizeType kSustract(SizeType k1, SizeType k2) const
 	{
+		if (!enabled_) printErrorAndDie("kSustract");
 		return (k1^k2);
 	}
 
+private:
+
+	void printErrorAndDie(PsimagLite::String func) const
+	{
+		PsimagLite::String str("GeometryDca:: Only valid for 2x2 clusters\n");
+		str += "Not for " + message_ + " orbitals.\n";
+		str += "\tWhile calling function " + func + "\n";
+		throw PsimagLite::RuntimeError(str);
+	}
+
+	bool enabled_;
+	PsimagLite::String message_;
 }; // class GeometryDca
 
-} // namespace PsimagLite 
+} // namespace PsimagLite
 
 #endif
 
