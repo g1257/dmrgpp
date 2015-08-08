g1257/dmrgpp

commit 816645290f8a77aae2ae11d8b5fc97adfde47b24

VectorWithOffset operator+=

src/Engine/VectorWithOffset.h
@@ -258,6 +258,11 @@ public:
 
 	VectorWithOffset<FieldType> operator+=(const VectorWithOffset<FieldType>& v)
 	{
+		if (size_ == 0 && offset_ == 0 && m_ == 0) {
+			data_ = v.data_;
+			return *this;
+		}
+
 		if (size_ != v.size_ || offset_ != v.offset_ || m_ != v.m_)
 			throw PsimagLite::RuntimeError("VectorWithOffset::operator+=\n");
 

commit d27bae9b5d7ef4521ab10f21efaabfaef30471ac

fixes bug introduced in 816645290f8a77aae2ae11d8b5fc97adfde47b24

src/Engine/ApplyOperatorExpression.h
@@ -477,6 +477,19 @@ private:
 				timeVectorsBase_->timeHasAdvanced();
 			}
 
+			if (targetVectors_.size() <= advance) {
+				PsimagLite::String s(__FILE__);
+				s += ": TargetVectors.size()" + ttos(targetVectors_.size());
+				s += " but advance=" + ttos(advance) + "\n";
+				throw PsimagLite::RuntimeError(s);
+			}
+
+			if (targetVectors_[advance].size() == 0) {
+				PsimagLite::String s(__FILE__);
+				s += ": TargetVectors[" + ttos(advance) + "].size()==0\n";
+				throw PsimagLite::RuntimeError(s);
+			}
+
 			if (site==0 || site==numberOfSites -1)  {
 				// don't wft since we did it before
 				assert(advance < targetVectors_.size());

src/Engine/TargetingTimeStep.h
@@ -203,6 +203,7 @@ public:
 	            const BlockType&,
 	            SizeType loopNumber)
 	{
+		if (direction == ProgramGlobals::INFINITE) return;
 		VectorWithOffsetType phiNew;
 		this->common().getPhi(phiNew,Eg,direction,block1[0],loopNumber);
 

src/Engine/VectorWithOffset.h
@@ -280,6 +280,9 @@ public:
 	{
 		if (size_ == 0 && offset_ == 0 && m_ == 0) {
 			data_ = v.data_;
+			size_ = v.size_;
+			offset_ = v.offset_;
+			m_ = v.m_;
 			return *this;
 		}
 