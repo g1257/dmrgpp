30ad9cafe0884d79d282c9236fbb6fdbf84d5bcc
g1257 <gz1@ornl.gov>
2016-01-29 20:39:54 -0500

    observe code compiles and links

--- src/configure.pl
+++ src/configure.pl
@@ -39,11 +39,14 @@ my %utilsDriver = (name => 'Utils', aux => 1);
 my %su2RelatedDriver = (name => 'Su2Related', aux => 1);
 my %toolboxDriver = (name => 'toolboxdmrg',
                      dotos => 'toolboxdmrg.o ProgramGlobals.o Provenance.o');
+my $dotos = "observe.o ProgramGlobals.o Provenance.o Utils.o Su2Related.o";
+my %observeDriver = (name => 'observe', dotos => $dotos);
+
 my @drivers = (\%provenanceDriver,\%su2RelatedDriver,
 \%progGlobalsDriver,\%restartDriver,\%finiteLoopDriver,\%utilsDriver,
-"observe",\%toolboxDriver);
+\%observeDriver,\%toolboxDriver);
 
-my $dotos = "dmrg.o Provenance.o RestartStruct.o FiniteLoop.o Utils.o ";
+$dotos = "dmrg.o Provenance.o RestartStruct.o FiniteLoop.o Utils.o ";
 $dotos .= " ProgramGlobals.o Su2Related.o";
 
 for (my $i = 0; $i < 83; ++$i) {
