g1257/dmrgpp

commit 5eef4acbdaa4372f37f9ddcf573d857fbe56da22

OPERATORS_DONT_CHANGE_ALL is now the default
To change basis even for un-needed operators say
CPPFLAGS += -DOPERATORS_CHANGE_ALL
in Config.make

src/Engine/Operators.h
@@ -189,7 +189,7 @@ public:
 
 		bool isExcluded(SizeType k) const
 		{
-#ifndef OPERATORS_DONT_CHANGE_ALL
+#ifdef OPERATORS_CHANGE_ALL
 			return false; // <-- this is the safest answer
 #endif
 			if (k < startEnd_.first || k >= startEnd_.second) return true;

TestSuite/inputs/Config.make
@@ -52,8 +52,8 @@ CPPFLAGS += -O3
 # This enables the custom allocator (use only for debugging)
 #CPPFLAGS += -DUSE_CUSTOM_ALLOCATOR
 
-#Don't change basis for un-needed operators (EXPERIMENTAL, NOT CHECKED)
-#CPPFLAGS += -DOPERATORS_DONT_CHANGE_ALL
+#Change basis even for un-needed operators
+#CPPFLAGS += -DOPERATORS_CHANGE_ALL
 
 # Specify the strip command to use (or use true to disable) 
 STRIP_COMMAND = strip 

TestSuite/inputs/ConfigCallgrind.make
@@ -52,8 +52,8 @@ CPPFLAGS +=   -g3
 # This enables the custom allocator (use only for debugging)
 #CPPFLAGS += -DUSE_CUSTOM_ALLOCATOR
 
-#Don't change basis for un-needed operators (EXPERIMENTAL, NOT CHECKED)
-#CPPFLAGS += -DOPERATORS_DONT_CHANGE_ALL
+#Change basis even for un-needed operators
+#CPPFLAGS += -DOPERATORS_CHANGE_ALL
 
 # Specify the strip command to use (or use true to disable) 
 STRIP_COMMAND = true

TestSuite/inputs/ConfigDebug.make
@@ -52,8 +52,8 @@ CPPFLAGS +=   -g3
 # This enables the custom allocator (use only for debugging)
 #CPPFLAGS += -DUSE_CUSTOM_ALLOCATOR
 
-#Don't change basis for un-needed operators (EXPERIMENTAL, NOT CHECKED)
-#CPPFLAGS += -DOPERATORS_DONT_CHANGE_ALL
+#Change basis even for un-needed operators
+#CPPFLAGS += -DOPERATORS_CHANGE_ALL
 
 # Specify the strip command to use (or use true to disable) 
 STRIP_COMMAND = true

TestSuite/inputs/ConfigHelgrind.make
@@ -52,8 +52,8 @@ CPPFLAGS +=   -g3
 # This enables the custom allocator (use only for debugging)
 #CPPFLAGS += -DUSE_CUSTOM_ALLOCATOR
 
-#Don't change basis for un-needed operators (EXPERIMENTAL, NOT CHECKED)
-#CPPFLAGS += -DOPERATORS_DONT_CHANGE_ALL
+#Change basis even for un-needed operators
+#CPPFLAGS += -DOPERATORS_CHANGE_ALL
 
 # Specify the strip command to use (or use true to disable) 
 STRIP_COMMAND = true

src/Config.make.sample
@@ -52,8 +52,8 @@ CPPFLAGS += -O3
 # This enables the custom allocator (use only for debugging)
 #CPPFLAGS += -DUSE_CUSTOM_ALLOCATOR
 
-#Don't change basis for un-needed operators (EXPERIMENTAL, NOT CHECKED)
-#CPPFLAGS += -DOPERATORS_DONT_CHANGE_ALL
+#Change basis even for un-needed operators
+#CPPFLAGS += -DOPERATORS_CHANGE_ALL
 
 # Specify the strip command to use (or use true to disable) 
 STRIP_COMMAND = strip 

commit be0ea569f506f4f88095b42a83846b6813a9df9d

fixes bug introduced in 5eef4acbdaa4372f37f9ddcf573d857fbe56da22
Because not all operators are changed, and operators
that are not changed are cleared, care needs
to be taken when running SU(2) so that non-existing operators
are not cleared.

src/Engine/Operators.h
@@ -161,7 +161,7 @@ public:
 				if (taskNumber>=total) break;
 
 				SizeType k = taskNumber;
-				if (isExcluded(k)) {
+				if (isExcluded(k) && k < operators_.size()) {
 					operators_[k].data.clear();
 					continue;
 				}