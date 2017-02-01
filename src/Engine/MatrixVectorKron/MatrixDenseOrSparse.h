f297eb5b848876fc9584587b5db00d9ed8ae2d54
Gonzalo Alvarez <gz1@ornl.gov>
2017-02-01 13:42:31 -0500

    MatrixVectorKron directory

---
+++ src/Engine/MatrixVectorKron/MatrixDenseOrSparse.h
@@ -0,0 +1,12 @@
+#ifndef MATRIXDENSEORSPARSE_H
+#define MATRIXDENSEORSPARSE_H
+
+namespace Dmrg {
+
+
+template<typename ComplexOrRealType>
+class MatrixDenseOrSparse {
+
+}; // class MatrixDenseOrSparse
+} // namespace Dmrg
+#endif // MATRIXDENSEORSPARSE_H
