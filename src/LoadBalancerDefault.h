449192b292e4120c7a0feeebfa6b8187029ea3bd
Gonzalo Alvarez <gz1@ornl.gov>
2017-02-01 18:45:43 -0500

    added BalancerDefault and NoPthreadsNg
    There are two interfaces to [No]Pthreads[Ng]:
    the old one, and the new one with Ng.
    For now, the old one is the default.
    The new one can be enabled with
    in your driver before you include
    Parallelizer.h
    After a period of testing, the new (Ng) interface
    will become the default

---
+++ src/LoadBalancerDefault.h
@@ -0,0 +1,61 @@
+#ifndef LOADBALANCERDEFAULT_H
+#define LOADBALANCERDEFAULT_H
+#include "Vector.h"
+
+namespace PsimagLite {
+
+class LoadBalancerDefault {
+
+public:
+
+	typedef PsimagLite::Vector<SizeType>::Type VectorSizeType;
+
+	LoadBalancerDefault(const VectorSizeType& weights, SizeType nthreads)
+	    : taskNumber_(nthreads)
+	{
+		VectorSizeType workLoad(nthreads, 0);
+
+		SizeType ntasks = weights.size();
+		for (SizeType i = 0; i < ntasks; ++i) {
+			SizeType thread = findThreadWithLightestWork(workLoad);
+			// assign work to thread
+			assert(thread < taskNumber_.size());
+			taskNumber_[thread].push_back(i);
+			// update work loads
+			assert(thread < workLoad.size());
+			workLoad[thread] += weights[i];
+		}
+
+		for (SizeType i = 0; i < nthreads; ++i) {
+			SizeType n = taskNumber_[i].size();
+			std::cout<<n<<" Indices allocated to thread "<<i<<": ";
+			for (SizeType j = 0; j < n; ++j)
+				std::cout<<taskNumber_[i][j]<<" ";
+			std::cout<<"\n";
+		}
+	}
+
+	SizeType blockSize(SizeType threadNum) const
+	{
+		assert(threadNum < taskNumber_.size());
+		return taskNumber_[threadNum].size();
+	}
+
+	int taskNumber(SizeType threadNum, SizeType p) const
+	{
+		assert(threadNum < taskNumber_.size());
+		assert(p < taskNumber_[threadNum].size());
+		return taskNumber_[threadNum][p];
+	}
+
+private:
+
+	SizeType findThreadWithLightestWork(const VectorSizeType& workLoad) const
+	{
+		return std::min_element(workLoad.begin(), workLoad.end()) - workLoad.begin();
+	}
+
+	PsimagLite::Vector<VectorSizeType>::Type taskNumber_;
+}; // class LoadBalancerDefault
+} // namespace PsimagLite
+#endif // LOADBALANCERDEFAULT_H
---
+++ src/NoPthreadsNg.h
@@ -0,0 +1,135 @@
+/*
+Copyright (c) 2009-2017, UT-Battelle, LLC
+All rights reserved
+
+[PsimagLite, Version 1.]
+[by G.A., Oak Ridge National Laboratory]
+
+UT Battelle Open Source Software License 11242008
+
+OPEN SOURCE LICENSE
+
+Subject to the conditions of this License, each
+contributor to this software hereby grants, free of
+charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), a
+perpetual, worldwide, non-exclusive, no-charge,
+royalty-free, irrevocable copyright license to use, copy,
+modify, merge, publish, distribute, and/or sublicense
+copies of the Software.
+
+1. Redistributions of Software must retain the above
+copyright and license notices, this list of conditions,
+and the following disclaimer.  Changes or modifications
+to, or derivative works of, the Software should be noted
+with comments and the contributor and organization's
+name.
+
+2. Neither the names of UT-Battelle, LLC or the
+Department of Energy nor the names of the Software
+contributors may be used to endorse or promote products
+derived from this software without specific prior written
+permission of UT-Battelle.
+
+3. The software and the end-user documentation included
+with the redistribution, with or without modification,
+must include the following acknowledgment:
+
+"This product includes software produced by UT-Battelle,
+LLC under Contract No. DE-AC05-00OR22725  with the
+Department of Energy."
+
+---------------------------------------------------------------
+DISCLAIMER
+
+THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER, CONTRIBUTORS, UNITED STATES GOVERNMENT,
+OR THE UNITED STATES DEPARTMENT OF ENERGY BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
+STATES DEPARTMENT OF ENERGY, NOR THE COPYRIGHT OWNER, NOR
+ANY OF THEIR EMPLOYEES, REPRESENTS THAT THE USE OF ANY
+INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+
+---------------------------------------------------------------
+
+*/
+/** \ingroup PsimagLite */
+/*@{*/
+
+/*! \file NoPthreadsNg .h
+ *
+ *  A C++ NoPthreadsNg class that implements the Concurrency interface
+ *
+ */
+#ifndef PSI_NOPTHREADS_NG_H
+#define PSI_NOPTHREADS_NG_H
+#include "LoadBalancerDefault.h"
+
+namespace PsimagLite {
+
+template<typename PthreadFunctionHolderType, typename LoadBalancerType=LoadBalancerDefault>
+class NoPthreadsNg  {
+
+public:
+
+	typedef LoadBalancerDefault::VectorSizeType VectorSizeType;
+
+	NoPthreadsNg(SizeType nNoPthreadsNg,int = 0)
+	{
+		if (nNoPthreadsNg != 1)
+			throw PsimagLite::RuntimeError("NoPthreadsNg: ctor with threads != 1\n");
+	}
+
+	// no weights, no balancer ==> create weights, set all weigths to 1, delegate
+	void loopCreate(PthreadFunctionHolderType& pfh)
+	{
+		SizeType ntasks = pfh.tasks();
+		VectorSizeType weights(ntasks,1);
+		loopCreate(pfh,weights);
+	}
+
+	// weights, no balancer ==> create balancer with weights ==> delegate
+	void loopCreate(PthreadFunctionHolderType& pfh, const VectorSizeType& weights)
+	{
+		LoadBalancerType* loadBalancer = new LoadBalancerType(weights, 1);
+		loopCreate(pfh,*loadBalancer);
+		delete loadBalancer;
+		loadBalancer = 0;
+	}
+
+	// balancer (includes weights)
+	void loopCreate(PthreadFunctionHolderType& pfh,
+	                const LoadBalancerType&)
+	{
+		SizeType ntasks = pfh.tasks();
+		for (SizeType taskNumber = 0; taskNumber < ntasks; ++taskNumber) {
+			pfh.doTask(taskNumber, 0);
+		}
+	}
+
+	String name() const { return "NoPthreadsNg"; }
+
+	SizeType threads() const { return 1; }
+
+	SizeType mpiProcs() const { return 1; }
+}; // NoPthreadsNg class
+
+} // namespace Dmrg
+
+/*@}*/
+#endif
