525434901a36cb1cf4c78ca29d794031c847a4e8
Gonzalo Alvarez <gz1@ornl.gov>
2017-02-16 14:14:32 -0500

    ContinuedFraction: matsubaras fixed

--- src/ContinuedFraction.h
+++ src/ContinuedFraction.h
@@ -221,11 +221,7 @@ private:
 
 	RealType matsubara(int ind,const PlotParamsType& params) const
 	{
-		int halfNs = static_cast<int>(params.omega1);
-		RealType factor = 2.0*M_PI/params.beta;
-		int ind2 = ind - halfNs;
-		if (ind2 >= 0) return factor*(ind2 + 1);
-		return factor*ind2;
+		return (2.0*ind + 1.0)*M_PI/params.beta;
 	}
 
 	ProgressIndicator progress_;
