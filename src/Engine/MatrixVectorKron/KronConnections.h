0b6322cda831ac2fec13a89cae0b9abc77ffc3b8
Gonzalo Alvarez <gz1@ornl.gov>
2017-02-17 14:12:25 -0500

    KronConnections: added value of connection

--- src/Engine/MatrixVectorKron/KronConnections.h
+++ src/Engine/MatrixVectorKron/KronConnections.h
@@ -168,6 +168,7 @@ public:
 			for (SizeType k = 0; k < sizeListK; ++k) {
 				const MatrixDenseOrSparseType& Ak = initKron_.xc(k)(outPatch, inPatch);
 				const MatrixDenseOrSparseType& Bk = initKron_.yc(k)(outPatch, inPatch);
+				const ComplexOrRealType& val = initKron_.value(k);
 
 				if (Ak.isZero() || Bk.isZero())
 					continue;
@@ -182,11 +183,11 @@ public:
 
 					kronMult(yi2, xi, 't', 't', Ak, Bk);
 					for (SizeType j = j1; j < j2; j++)
-						y_[j] += yi2[j-j1];
+						y_[j] += yi2[j-j1]*val;
 				} else {
 					kronMult(yi2, xj, 'n','n', Ak, Bk);
 					for (SizeType i = 0; i < vsize_[outPatch]; ++i)
-						yij[i] += yi2[i];
+						yij[i] += yi2[i]*val;
 				}
 			} // end loop over k
 
