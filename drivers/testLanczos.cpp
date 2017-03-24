9023c64c488c95a6f4b8debcdab913ceb4a041af
Gonzalo Alvarez <gz1@ornl.gov>
2017-03-24 14:47:56 -0400

    testLanzcos added

---
+++ drivers/testLanczos.cpp
@@ -0,0 +1,32 @@
+#include "LanczosSolver.h"
+#include "Matrix.h"
+#include "CrsMatrix.h"
+
+int main()
+{
+	typedef std::complex<double> ComplexType;
+	typedef PsimagLite::ParametersForSolver<double> SolverParametersType;
+	typedef PsimagLite::Vector<ComplexType>::Type VectorType;
+
+	int n = 8;
+	PsimagLite::Matrix<ComplexType> m(n, n);
+	// fill m
+	for (int i = 0; i < n; ++i) m(i,i) = 1.0;
+	m(1,2) = ComplexType(0.0,0.5);
+	m(2,1) = PsimagLite::conj(m(1,2));
+	m(3,5) = m(5,3) = -1.5;
+
+	PsimagLite::CrsMatrix<ComplexType> msparse(m);
+
+	SolverParametersType params;
+
+	PsimagLite::LanczosSolver<SolverParametersType,
+	        PsimagLite::CrsMatrix<ComplexType>,VectorType> lanczosSolver(msparse, params);
+
+	double e = 0;
+	VectorType z(n, 0.0);
+	lanczosSolver.computeGroundState(e, z);
+
+	std::cout<<"energy="<<e<<"\n";
+	std::cout<<z<<"\n";
+}
