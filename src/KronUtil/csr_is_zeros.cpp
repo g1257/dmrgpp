4a2ee42f5de13a05a957ccc9ab2fc1d7cb4f64c8
Gonzalo Alvarez <gz1@ornl.gov>
2017-05-26 20:30:42 -0400

    KronUtil: performance fix

--- src/KronUtil/csr_is_zeros.cpp
+++ src/KronUtil/csr_is_zeros.cpp
@@ -3,30 +3,9 @@
 template<typename ComplexOrRealType>
 bool csr_is_zeros(const PsimagLite::CrsMatrix<ComplexOrRealType>& a)
 {
-   // ----------------------------------------------------
-   // check whether a sparse matrix is the zero matrix
-   // ----------------------------------------------------
-        
-   const ComplexOrRealType zero = 0;
-   const int nrow_A = a.row();
-   const int ncol_A = a.col();
-
-
-
-   for(int ia=0; ia < nrow_A; ia++) {
-     int istart = a.getRowPtr(ia);
-     int iend = a.getRowPtr(ia + 1);
-     for(int k=istart; k < iend; k++) {
-         int ja = a.getCol(k);
- 
-         bool isok = ((0 <= ja) && (ja < ncol_A)); 
-         assert( isok );
-
-         ComplexOrRealType aij = a.getValue(k);
-         if (isok && (aij != zero)) { return( false ); };
-       };
-    };
-
-   return( true );
+	// ----------------------------------------------------
+	// check whether a sparse matrix is the zero matrix
+	// ----------------------------------------------------
+	return isZero(a, 1e-12);
 }
- 
+
