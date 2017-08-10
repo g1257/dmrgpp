a058163b2a26f022e5f6a11854b19b26bb07006d
Gonzalo Alvarez <gz1@ornl.gov>
2017-08-10 15:14:01 -0400

    timeObservablesInSitu perl module

---
+++ scripts/timeObservablesInSitu.pm
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+package timeObservablesInSitu;
+
+sub main
+{
+	my ($site, $label, $fin, $fout)=@_;
+	defined($fout) or return;
+
+	my $counter=0;
+	print $fout "#site= $site\n";
+	print $fout "#label=$label\n";
+
+	while (<$fin>) {
+		chomp;
+		next unless /\Q$label/;
+		if (/^${site} /) {
+			my @temp = split;
+			my $value = procValue($temp[1]);
+			my $time = $temp[2];
+			my $superdensity = procValue($temp[4]);
+			print $fout "$time  $value $superdensity\n";
+		}
+	}
+}
+
+sub procValue
+{
+	my ($t)=@_;
+	$_=$t;
+	s/\(//;
+	s/,.*$//;
+	return $_;
+}
+
+1;
