934890c67d5debc294bbc5a2b349b051954f5b13
Gonzalo Alvarez <gz1@ornl.gov>
2017-12-12 18:13:51 -0500

    WftAccelWithTemp in separate class

--- src/Engine/Wft/WaveFunctionTransfLocal.h
+++ src/Engine/Wft/WaveFunctionTransfLocal.h
@@ -90,6 +90,7 @@ DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
 #include "MatrixVectorKron/InitKronWft.h"
 #include "MatrixVectorKron/KronMatrix.h"
 #include "WftAccelBlocks.h"
+#include "WftAccelWithTemp.h"
 
 namespace Dmrg {
 
@@ -120,12 +121,14 @@ public:
 	typedef InitKronWft<LeftRightSuperType, WftOptions, DmrgWaveStructType> InitKronType;
 	typedef PsimagLite::Matrix<SparseElementType> MatrixType;
 	typedef WftAccelBlocks<BaseType> WftAccelBlocksType;
+	typedef WftAccelWithTemp<BaseType, MatrixOrIdentityType> WftAccelWithTempType;
 
 	WaveFunctionTransfLocal(const DmrgWaveStructType& dmrgWaveStruct,
 	                        const WftOptions& wftOptions)
 	    : dmrgWaveStruct_(dmrgWaveStruct),
 	      wftOptions_(wftOptions),
 	      wftAccelBlocks_(dmrgWaveStruct, wftOptions),
+	      wftAccelWithTemp_(dmrgWaveStruct, wftOptions),
 	      progress_("WaveFunctionTransfLocal")
 	{
 		PsimagLite::OstringStream msg;
@@ -250,7 +253,7 @@ private:
 	                          const VectorSizeType& nk) const
 	{
 		if (wftOptions_.accel == WftOptions::ACCEL_TEMP)
-			return transformTemp1FromInfinite(psiDest, psiSrc, lrs, i0, nk);
+			return wftAccelWithTemp_.environFromInfinite(psiDest, psiSrc, lrs, i0, nk);
 
 		if (wftOptions_.accel == WftOptions::ACCEL_BLOCKS &&
 		        lrs.left().block().size() > 1)
@@ -314,81 +317,6 @@ private:
 		return sum;
 	}
 
-	void transformTemp1FromInfinite(VectorWithOffsetType& psiDest,
-	                                const VectorWithOffsetType& psiSrc,
-	                                const LeftRightSuperType& lrs,
-	                                SizeType i0,
-	                                const VectorSizeType& nk) const
-	{
-		SizeType volumeOfNk = DmrgWaveStructType::volumeOf(nk);
-		SizeType nip = lrs.super().permutationInverse().size()/
-		        lrs.right().permutationInverse().size();
-
-		SizeType nip2 = dmrgWaveStruct_.lrs.left().size()/volumeOfNk;
-
-		assert(lrs.left().permutationInverse().size()==volumeOfNk ||
-		       lrs.left().permutationInverse().size()==dmrgWaveStruct_.ws.rows());
-		assert(lrs.right().permutationInverse().size()/volumeOfNk==dmrgWaveStruct_.we.cols());
-
-		SizeType start = psiDest.offset(i0);
-		SizeType total = psiDest.effectiveSize(i0);
-
-		SparseMatrixType ws;
-		dmrgWaveStruct_.ws.toSparse(ws);
-		SparseMatrixType we;
-		dmrgWaveStruct_.we.toSparse(we);
-		SparseMatrixType weT;
-		transposeConjugate(weT,we);
-
-		MatrixOrIdentityType wsRef2(wftOptions_.twoSiteDmrg && nip>volumeOfNk,ws);
-
-		MatrixType temporary;
-		buildTemporaryEnviron(temporary, psiSrc, i0, we);
-
-		PackIndicesType pack1(nip);
-		PackIndicesType pack2(volumeOfNk);
-		for (SizeType x=0;x<total;x++) {
-			SizeType ip = 0;
-			SizeType beta = 0;
-			pack1.unpack(ip,beta,(SizeType)lrs.super().permutation(x+start));
-			SizeType kp = 0;
-			SizeType jp = 0;
-			pack2.unpack(kp, jp, lrs.right().permutation(beta));
-			for (SizeType k3=wsRef2.getRowPtr(ip);k3<wsRef2.getRowPtr(ip+1);k3++) {
-				int ip2 = wsRef2.getColOrExit(k3);
-				if (ip2<0) continue;
-				SizeType alpha = dmrgWaveStruct_.lrs.left().permutationInverse(ip2+kp*nip2);
-				psiDest.fastAccess(i0,x) += temporary(jp, alpha)*wsRef2.getValue(k3);
-			}
-		}
-	}
-
-	void buildTemporaryEnviron(MatrixType& temporary,
-	                           const VectorWithOffsetType& psiV,
-	                           SizeType i0,
-	                           const SparseMatrixType& we) const
-	{
-		SizeType ni=dmrgWaveStruct_.lrs.left().size();
-		temporary.resize(we.cols(), ni);
-		temporary.setTo(0.0);
-
-		PackIndicesType packSuper(ni);
-		SizeType total = psiV.effectiveSize(i0);
-		SizeType offset = psiV.offset(i0);
-
-		for (SizeType x = 0; x < total; ++x) {
-			SizeType alpha = 0;
-			SizeType jp2 = 0;
-			packSuper.unpack(alpha, jp2, dmrgWaveStruct_.lrs.super().permutation(x + offset));
-			SizeType start = we.getRowPtr(jp2);
-			SizeType end = we.getRowPtr(jp2+ 1);
-			for (SizeType k = start; k < end; k++) {
-				SizeType jp = we.getCol(k);
-				temporary(jp, alpha) += psiV.fastAccess(i0, x)*we.getValue(k);
-			}
-		}
-	}
-
 	void transformVector2(VectorWithOffsetType& psiDest,
 	                      const VectorWithOffsetType& psiSrc,
 	                      const LeftRightSuperType& lrs,
@@ -432,7 +360,7 @@ private:
 				VectorType dest(final-start,0.0);
 				if (srcI > 0) psiDest.extract(dest,i0);
 				if (wftOptions_.accel == WftOptions::ACCEL_TEMP) {
-					transformTemp2FromInfinite(dest,
+					wftAccelWithTemp_.systemFromInfinite(dest,
 					                           start,
 					                           psiV,
 					                           offset,
@@ -520,73 +448,6 @@ private:
 		}
 
 		return sum;
-	}
-
-	void transformTemp2FromInfinite(VectorType& dest,
-	                                SizeType destOffset,
-	                                const VectorType& psiV,
-	                                SizeType offset,
-	                                const LeftRightSuperType& lrs,
-	                                const VectorSizeType& nk,
-	                                const SparseMatrixType& ws,
-	                                const SparseMatrixType& we) const
-	{
-		SizeType volumeOfNk = DmrgWaveStructType::volumeOf(nk);
-		SizeType nip = lrs.left().permutationInverse().size()/volumeOfNk;
-		SizeType nalpha = lrs.left().permutationInverse().size();
-		SizeType ni = dmrgWaveStruct_.lrs.right().size()/volumeOfNk;
-		MatrixOrIdentityType weRef(wftOptions_.twoSiteDmrg && ni>volumeOfNk, we);
-
-		assert(nip==dmrgWaveStruct_.ws.cols());
-
-		MatrixType temporary;
-		buildTemporarySystem(temporary, psiV, offset, ws);
-
-		PackIndicesType pack1(nalpha);
-		PackIndicesType pack2(nip);
-		for (SizeType x=0;x<dest.size();x++) {
-			SizeType isn,jen;
-			pack1.unpack(isn,jen,(SizeType)lrs.super().permutation(x+destOffset));
-			SizeType is = 0;
-			SizeType jpl = 0;
-			pack2.unpack(is,jpl,(SizeType)lrs.left().permutation(isn));
-			SparseElementType sum = 0;
-			for (SizeType k2=weRef.getRowPtr(jen);k2<weRef.getRowPtr(jen+1);k2++) {
-				int jpr = weRef.getColOrExit(k2);
-				// jpr < 0 could be due to an m smaller than h, the Hilbert size of one site
-				// this is checked against elsewhere
-				assert(jpr >= 0);
-				SizeType jp = dmrgWaveStruct_.lrs.right().permutationInverse(jpl + jpr*volumeOfNk);
-				sum += temporary(is, jp)*weRef.getValue(k2);
-			}
-
-			dest[x] += sum;
-		}
-	}
-
-	void buildTemporarySystem(MatrixType& temporary,
-	                          const VectorType& psiV,
-	                          SizeType offset,
-	                          const SparseMatrixType& ws) const
-	{
-		SizeType nalpha=dmrgWaveStruct_.lrs.left().permutationInverse().size();
-		temporary.resize(nalpha, dmrgWaveStruct_.lrs.super().size()/nalpha);
-		temporary.setTo(0.0);
-
-		PackIndicesType packSuper(nalpha);
-		SizeType total = psiV.size();
-
-		for (SizeType y = 0; y < total; ++y) {
-			SizeType ip = 0;
-			SizeType jp = 0;
-			packSuper.unpack(ip, jp, dmrgWaveStruct_.lrs.super().permutation(y + offset));
-			SizeType start = ws.getRowPtr(ip);
-			SizeType end = ws.getRowPtr(ip + 1);
-			for (SizeType k = start;k < end; k++) {
-				SizeType is = ws.getCol(k);
-				temporary(is, jp) += ws.getValue(k)*psiV[y];
-			}
-		}
 	}
 
 	void transformVector1bounce(VectorWithOffsetType& psiDest,
@@ -714,6 +575,7 @@ private:
 	const DmrgWaveStructType& dmrgWaveStruct_;
 	const WftOptions& wftOptions_;
 	WftAccelBlocksType wftAccelBlocks_;
+	WftAccelWithTempType wftAccelWithTemp_;
 	PsimagLite::ProgressIndicator progress_;
 }; // class WaveFunctionTransfLocal
 } // namespace Dmrg
