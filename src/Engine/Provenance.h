9de72728fa5d6a0ca8364e60deb52a5223e3caf6
g1257 <gz1@ornl.gov>
2018-02-08 13:14:13 -0500

    version updated

--- src/Engine/Provenance.h
+++ src/Engine/Provenance.h
@@ -14,8 +14,8 @@ public:
 	static PsimagLite::String logo(PsimagLite::String appName)
 	{
 		PsimagLite::OstringStream msg;
-		msg<<appName<<"\x1b[38;5;124m";
-		msg<<" [features "<<DMRGPP_VERSION<<"] "<<PsimagLite::AnsiColor::reset;
+		msg<<appName<<"\x1b[38;5;240m";
+		msg<<" [master "<<DMRGPP_VERSION<<"] "<<PsimagLite::AnsiColor::reset;
 #ifdef PLUGIN_SC
 		msg<<"[PLUGIN_SC] ";
 #endif
