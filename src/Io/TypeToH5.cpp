20ff84973178c5a456a93cd747486666becb4dd7
Gonzalo Alvarez <gz1@ornl.gov>
2018-04-20 15:10:28 -0400

    IoNg: native data types for H5 now

--- src/Io/TypeToH5.cpp
+++ src/Io/TypeToH5.cpp
@@ -5,25 +5,38 @@
 namespace PsimagLite {
 
 template<>
- H5::PredType typeToH5<double>() { return H5::PredType::NATIVE_DOUBLE; }
-
-template<>
- H5::PredType typeToH5<unsigned int>() { return H5::PredType::NATIVE_UINT8; }
-
-template<>
- H5::PredType typeToH5<int>() { return H5::PredType::NATIVE_INT8; }
-
-template<>
- H5::PredType typeToH5<unsigned long>() { return H5::PredType::NATIVE_UINT16; }
-
-template<>
- H5::PredType typeToH5<long>() { return H5::PredType::NATIVE_INT16; }
-
-template<>
- H5::PredType typeToH5<bool>() { return H5::PredType::NATIVE_HBOOL; }
-
-template<>
 H5::PredType typeToH5<char>() { return H5::PredType::NATIVE_CHAR; }
+
+template<>
+H5::PredType typeToH5<bool>() { return H5::PredType::NATIVE_HBOOL; }
+
+template<>
+H5::PredType typeToH5<int>() { return H5::PredType::NATIVE_INT; }
+
+template<>
+H5::PredType typeToH5<short>() { return H5::PredType::NATIVE_SHORT; }
+
+template<>
+H5::PredType typeToH5<long>() { return H5::PredType::NATIVE_LONG; }
+
+template<>
+H5::PredType typeToH5<unsigned int>() { return H5::PredType::NATIVE_UINT; }
+
+template<>
+H5::PredType typeToH5<unsigned short>() { return H5::PredType::NATIVE_USHORT; }
+
+template<>
+H5::PredType typeToH5<unsigned long>() { return H5::PredType::NATIVE_ULONG; }
+
+template<>
+H5::PredType typeToH5<float>() { return H5::PredType::NATIVE_FLOAT; }
+
+template<>
+H5::PredType typeToH5<double>() { return H5::PredType::NATIVE_DOUBLE; }
+
+
+
+
 } // namespace PsimagLite
 #endif
 
