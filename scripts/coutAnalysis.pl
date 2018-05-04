eb9aef1d3de5aead8edc273b0ea34d76c55d3e36
Gonzalo Alvarez <gz1@ornl.gov>
2018-05-04 14:45:00 -0400

    coutAnalysis.pl: cutoff=0 by default, prints totals

--- scripts/coutAnalysis.pl
+++ scripts/coutAnalysis.pl
@@ -12,24 +12,29 @@ use utf8;
 
 my ($file, $cutoff) = @ARGV;
 defined($file) or die "USAGE: $0 file\n";
-defined($cutoff) or $cutoff = 2;
+defined($cutoff) or $cutoff = 0;
 
 my %h;
-loadData(\%h, $file, $cutoff);
+my $totalTime = loadData(\%h, $file, $cutoff);
 
+print STDERR "#cutoff=$cutoff\n";
 print STDERR "#Elements=".scalar(keys %h)."\n";
 printData(\%h);
+print STDERR "#TotalForRun=$totalTime\n";
 
 sub loadData
 {
 	my ($h, $file, $cutoff) = @_;
 	open(FILE, "<", $file) or die "$0: Cannot open $file : $!\n";
 	my $prevt = 0;
+	my ($t0, $t1);
 	while (<FILE>) {
 		my $x = /^(.+) *\[(\d+)\]\:/;
 		next unless ($x);
 		my $name = $1;
 		my $t = $2;
+		$t0 = $t if (!defined($t0));
+		$t1 = $t;
 		my $dt = $t - $prevt;
 		$prevt = $t;
 		next if ($dt < $cutoff);
@@ -41,6 +46,7 @@ sub loadData
 	}
 
 	close(FILE);
+	return $t1 - $t0;
 }
 
 
@@ -48,9 +54,14 @@ sub printData
 {
 	my ($hptr) = @_;
 	my %h = %$hptr;
+	my $tot = 0;
 	foreach my $k (sort {$h{$b} <=> $h{$a}} keys %h) {
 		my $t = $hptr->{$k};
 		print "$k $t\n";
+		$tot += $t;
 	}
+
+	print STDERR "--------------------\n";
+	print STDERR "TotalForModules: $tot\n";
 }
 
