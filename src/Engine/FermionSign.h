g1257/dmrgpp

commit 86dbfb2afb9d8063222d25dced7bb3c263594ad6

Basis: signs old instead of electrons old

src/Engine/FermionSign.h
@@ -88,6 +88,7 @@ class FermionSign {
 
 	typedef PsimagLite::PackIndices PackIndicesType;
 	typedef PsimagLite::Vector<SizeType>::Type VectorSizeType;
+	typedef PsimagLite::Vector<bool>::Type VectorBoolType;
 
 public:
 
@@ -100,24 +101,22 @@ public:
 	template<typename SomeBasisType>
 	FermionSign(const SomeBasisType& basis,const VectorSizeType& electrons)
 	{
+		if (basis.oldSigns().size() != basis.permutationInverse().size())
+			err("FermionSign: Problem\n");
 
-		const VectorSizeType& basisElectrons =
-		        basis.electronsVector(SomeBasisType::BEFORE_TRANSFORM);
-		if (basisElectrons.size()!=basis.permutationInverse().size())
-			throw PsimagLite::RuntimeError("Problem\n");
-		SizeType nx = basisElectrons.size()/electrons.size();
-		VectorSizeType el(nx);
+		SizeType n = basis.oldSigns().size();
+		SizeType nx = basis.oldSigns().size()/electrons.size();
 		PackIndicesType pack(nx);
-		for (SizeType x=0;x<basisElectrons.size();x++) {
-			SizeType x0,x1;
-			pack.unpack(x0,x1,basis.permutation(x));
-			assert(x1<electrons.size());
-			int nx0 = basisElectrons[x]-electrons[x1];
-			assert(nx0>=0 && x0<el.size());
-			el[x0] = nx0;
-		}
 
-		init(el);
+		for (SizeType x = 0; x < n; ++x) {
+			SizeType x0 = 0;
+			SizeType x1 = 0;
+			pack.unpack(x0, x1, basis.permutation(x));
+			assert(x1 < electrons.size());
+			bool parity1 = basis.oldSigns()[x];
+			bool parity2 = (electrons[x1] & 1);
+			signs_[x0] = (parity1 != parity2);
+		}
 	}
 
 	template<typename IoInputter>
@@ -168,7 +167,7 @@ private:
 			signs_[i] = (electrons[i] & 1);
 	}
 
-	PsimagLite::Vector<bool>::Type signs_;
+	VectorBoolType signs_;
 }; // class FermionSign
 } // namespace Dmrg
 

src/Engine/Basis.h
@@ -107,8 +107,7 @@ public:
 	typedef VectorSizeType BlockType;
 	typedef SparseMatrixType_ SparseMatrixType;
 	typedef RealType_ RealType;
-
-	enum WhenTransformEnum {BEFORE_TRANSFORM, AFTER_TRANSFORM};
+	typedef PsimagLite::Vector<bool>::Type VectorBoolType;
 
 	enum {SAVE_ALL, SAVE_PARTIAL};
 
@@ -156,7 +155,7 @@ public:
 		VectorSizeType qns;
 		basisData.findQuantumNumbers(qns, useSu2Symmetry_);
 		findPermutationAndPartition(qns, true);
-		electronsOld_=electrons_;
+		electronsToSigns(electrons_);
 		shrinkVector(qns_, qns, partition_);
 	}
 
@@ -237,7 +236,7 @@ public:
 		// order quantum numbers of combined basis:
 		findPermutationAndPartition(qns, true);
 		reorder();
-		electronsOld_ = electrons_;
+		electronsToSigns(electrons_);
 		shrinkVector(qns_, qns, partition_);
 	}
 
@@ -419,11 +418,9 @@ public:
 	}
 
 	//! Returns the vector of electrons for this basis
-	const VectorSizeType& electronsVector(WhenTransformEnum beforeOrAfterTransform) const
-	{
-		return (beforeOrAfterTransform == AFTER_TRANSFORM) ? electrons_ :
-		                                                     electronsOld_;
-	}
+	const VectorSizeType& electronsVector() const { return electrons_; }
+
+	const VectorBoolType& oldSigns() const { return signsOld_; }
 
 	//! Returns the fermionic sign for state i
 	int fermionicSign(SizeType i,int f) const
@@ -495,7 +492,7 @@ public:
 
 		if (!minimizeWrite) {
 			io.write(electrons_,"ELECTRONS");
-			io.write(electronsOld_,"0OLDELECTRONS");
+			io.write(signsOld_,"SignsOld");
 		}
 
 		io.write(partition_,"PARTITION");
@@ -520,7 +517,7 @@ public:
 
 		if (!minimizeWrite) {
 			io.write(electrons_, label + "ELECTRONS");
-			io.write(electronsOld_, label + "0OLDELECTRONS");
+			io.write(signsOld_, label + "SignsOld");
 		}
 
 		io.write(partition_, label + "PARTITION");
@@ -615,7 +612,7 @@ private:
 
 		if (!minimizeRead) {
 			io.read(electrons_, prefix + "ELECTRONS");
-			io.read(electronsOld_, prefix + "0OLDELECTRONS");
+			io.read(signsOld_, prefix + "SignsOld");
 		}
 
 		io.read(partition_, prefix + "PARTITION");
@@ -711,6 +708,19 @@ private:
 		}
 	}
 
+	void electronsToSigns(const VectorSizeType& electrons)
+	{
+		SizeType n = electrons.size();
+		if (n == 0) {
+			signsOld_.clear();
+			return;
+		}
+
+		signsOld_.resize(n);
+		for (SizeType i = 0; i < n; ++i)
+			signsOld_[i] = (electrons[i] & 1);
+	}
+
 	void correctNameIfNeeded()
 	{
 		if (name_.find("/") == PsimagLite::String::npos)
@@ -750,7 +760,7 @@ these numbers are
 		*/
 	VectorSizeType qns_;
 	VectorSizeType electrons_;
-	VectorSizeType electronsOld_;
+	VectorBoolType signsOld_;
 
 	/* PSIDOC BasisPartition
 		What remains to be done is to find a partition of the basis which

src/Engine/BasisWithOperators.h
@@ -203,7 +203,7 @@ public:
 					const OperatorType& myOp =  basis2.getOperatorByIndex(i);
 					if (savedSign != myOp.fermionSign) {
 						utils::fillFermionicSigns(fermionicSigns,
-						                          basis2.electronsVector(BaseType::AFTER_TRANSFORM),
+						                          basis2.electronsVector(),
 						                          myOp.fermionSign);
 						savedSign = myOp.fermionSign;
 					}
@@ -227,7 +227,7 @@ public:
 
 					if (savedSign != myOp.fermionSign) {
 						utils::fillFermionicSigns(fermionicSigns,
-						                          basis2.electronsVector(BaseType::AFTER_TRANSFORM),
+						                          basis2.electronsVector(),
 						                          myOp.fermionSign);
 						savedSign = myOp.fermionSign;
 					}

src/Engine/CorrelationsSkeleton.h
@@ -290,7 +290,7 @@ private:
 
 	int fermionSignBasis(int fermionicSign, const BasisType& basis) const
 	{
-		const VectorSizeType& v = basis.electronsVector(BasisType::AFTER_TRANSFORM);
+		const VectorSizeType& v = basis.electronsVector();
 		SizeType nx0 = std::accumulate(v.begin(), v.end(), 0);
 		return (nx0 & 1) ? fermionicSign : 1;
 	}

src/Engine/DmrgSolver.h
@@ -590,12 +590,11 @@ private:
 	                                ProgramGlobals::DirectionEnum direction,
 	                                SizeType loopIndex)
 	{
-		bool twoSiteDmrg = (parameters_.options.find("twositedmrg")!=
-		        PsimagLite::String::npos);
-		const VectorSizeType& eS = pS.electronsVector(BasisWithOperatorsType::AFTER_TRANSFORM);
+		bool twoSiteDmrg = (parameters_.options.find("twositedmrg") != PsimagLite::String::npos);
+		const VectorSizeType& eS = pS.electronsVector();
 		FermionSignType fsS(eS);
 
-		const VectorSizeType& eE = pS.electronsVector(BasisWithOperatorsType::AFTER_TRANSFORM);
+		const VectorSizeType& eE = pS.electronsVector();
 		FermionSignType fsE(eE);
 
 		truncate_(pS,pE,target,keptStates,direction);

src/Engine/KroneckerDumper.h
@@ -82,7 +82,7 @@ public:
 		fout_<<"TargetElectronsUp="<<etarget.first<<"\n";
 		fout_<<"TargetElectronsDown="<<etarget.second<<"\n";
 
-		cacheSigns(lrs.left().electronsVector(BasisType::AFTER_TRANSFORM));
+		cacheSigns(lrs.left().electronsVector());
 		counter_++;
 	}
 
@@ -180,7 +180,7 @@ private:
 		}
 
 		fout_<<"Electrons\n";
-		fout_<<basis.electronsVector(BasisType::AFTER_TRANSFORM);
+		fout_<<basis.electronsVector();
 	}
 
 	PairSizeType getNupNdown(SizeType q, SizeType nOfQns) const

src/Engine/MatrixVectorKron/InitKronBase.h
@@ -116,7 +116,7 @@ public:
 	      ijpatchesNew_(&ijpatchesOld_),
 	      wftMode_(false)
 	{
-		cacheSigns(signsNew_, lrs.left().electronsVector(BasisType::AFTER_TRANSFORM));
+		cacheSigns(signsNew_, lrs.left().electronsVector());
 	}
 
 	InitKronBase(const LeftRightSuperType& lrsOld,
@@ -132,7 +132,7 @@ public:
 	      ijpatchesNew_(new GenIjPatchType(lrsNew, qn)),
 	      wftMode_(true)
 	{
-		cacheSigns(signsNew_, lrsNew.left().electronsVector(BasisType::AFTER_TRANSFORM));
+		cacheSigns(signsNew_, lrsNew.left().electronsVector());
 	}
 
 	~InitKronBase()

commit bc2a0a304ce4c3a36dd721861d4df3d5adf2b9cf

fixes bug introduced in 86dbfb2afb9d8063222d25dced7bb3c263594ad6

src/Engine/FermionSign.h
@@ -107,7 +107,7 @@ public:
 		SizeType n = basis.oldSigns().size();
 		SizeType nx = basis.oldSigns().size()/electrons.size();
 		PackIndicesType pack(nx);
-
+		signs_.resize(nx);
 		for (SizeType x = 0; x < n; ++x) {
 			SizeType x0 = 0;
 			SizeType x1 = 0;
@@ -115,6 +115,7 @@ public:
 			assert(x1 < electrons.size());
 			bool parity1 = basis.oldSigns()[x];
 			bool parity2 = (electrons[x1] & 1);
+			assert(x0 < signs_.size());
 			signs_[x0] = (parity1 != parity2);
 		}
 	}