30c0f1ce03da798751ea66fbe925e92f5ab68037
Gonzalo Alvarez <gz1@ornl.gov>
2018-06-27 21:52:57 -0400

    ci: kompile rig (again)

--- TestSuite/kompileRig.pl
+++ TestSuite/kompileRig.pl
@@ -12,15 +12,69 @@ my $counter = 0;
 $levels[$counter++] = ["", "CPPFLAGS += -DUSE_FLOAT"];
 $levels[$counter++] = ["", "CXX = clang++ -mtune=native"];
 
+my ($makeJ) = @ARGV;
+defined($makeJ) or die "USAGE: $0 makeJ\n";
+
 Combinatorial::main(\@levels, \&myCallback);
 
 sub myCallback
 {
 	my ($items) = @_;
-	my $n = scalar(@$items);
-	for (my $i = 0; $i < $n; ++$i) {
-		print "$items->[$i]\n";
-	}
+	
+	print flattenWithNewLines($items);
+
+	kompileRig($items);
 
 	print "---------------------\n";
 }
+
+sub kompileRig
+{
+	my ($items) = @_;
+	my $textToAppend = flattenWithNewLines($items);
+	my $command = "make clean; make -j $makeJ";
+	my @paths = ("../../PsimagLite/lib", "../../PsimagLite/drivers", "../src");
+	my $n = scalar(@paths);
+	for (my $i = 0; $i < $n; ++$i) {
+		kompileRigEach($paths[$i], $command, $textToAppend);
+	}
+}
+
+sub kompileRigEach
+{
+	my ($path, $command, $textToAppend) = @_;
+	unlink($path."/Config.make");
+	my $cmd = "cd $path; echo Y | perl configure.pl";
+	executeAndDieIfNotSuccess($cmd);
+	appendToFile($path."/Config.make", $textToAppend);
+	$cmd = "cd $path; $command";
+	executeAndDieIfNotSuccess($cmd);
+}
+
+sub flattenWithNewLines
+{
+	my ($items) = @_;
+	my $text = "";
+	my $n = scalar(@$items);
+	for (my $i = 0; $i < $n; ++$i) {
+		$text .= "$items->[$i]\n";
+	}
+
+	return $text;
+}
+
+sub appendToFile
+{
+	my ($file, $textToAppend) = @_;
+	open(FILE, ">>", $file) or die "$0: Cannot open $file : $!\n";
+	print FILE $textToAppend;
+	close(FILE);
+}
+
+sub executeAndDieIfNotSuccess
+{
+	my ($cmd) = @_;
+	system($cmd);
+	die "$0: Command $cmd FAILED\n" if ($? != 0);
+}
+
