623bbf47796a122eaff3455b1812c7ada815ae7a
Gonzalo Alvarez <gz1@ornl.gov>
2018-07-09 17:39:28 -0400

    PsiTag: spec updated

--- scripts/PsiTag.pm
+++ scripts/PsiTag.pm
@@ -5,28 +5,38 @@ use warnings;
 use utf8;
 
 =pod
- Syntax: tagging  mode content
- content can appear multiline if scope is block
- block is between ( ) multiline
- tagging and mode must appear in the same line
- automatic tagging is possible with .= content
- Mode is either
+Syntax: tagging  mode content
+content can appear multiline if scope is block
+block is between ( ) multiline
+tagging and mode must appear in the same line
+automatic tagging is possible with .= content
+Mode is either
              add definition and overwrite if it exits =
              add definition and append to it if it exits +=
              add definition or ignore if it exits =? 
              add definition or fail if it exits =!
              delete definition or ignore if it doesn't exist -=
              delete definition or fail if it exits -=!
-             output definition <
- tagging must not contain any of = + ? ! - < & *
+ 
+tagging must not contain any of = + ? ! - < & *
 
- Content is subject to interpretation as follows.
- First non-whitespace character after mode, if it is a ( in the same line
- Last  non-whitespace character in a line, if it is a )
- First non-whitespace character in a line, if it is a <
-    and what follows until newline is considered the tagging
- First non-whitespace character after mode, if it is a < in the same line
-    and what follows until newline is considered the tagging
+Content is subject to interpretation as follows.
+   
+   (1) First non-whitespace character after mode in the same line, if it is a (
+
+   (2) Last non-whitespace character in a line, if it is a )
+ 
+   (3) First non-whitespace character in a line or after mode, if it is a <
+       and what follows until newline is considered the tagging
+
+   (4) First non-whitespace character in line or after mode, if it is an escape \
+       will be removed. This is so that the next character will not be interpreted
+
+   (5) Last non-whitespace character in a line, if it is a \
+       will be removed. This is so that the previous character will not be interpreted
+
+Tagging is canonicalized as follows: See canonicalTagName below.
+
 =cut
 
 package PsiTag;
@@ -72,21 +82,26 @@ sub readTags
 				my $rest = $3;
 
 				my $thisLineParens = 0;
-				if ($rest =~ s/^[ \t]*\(//) {
+				if ($rest =~ s/^[ \t]*\(//) { # (1)
 					++$thisLineParens;
 				}
 
-				if ($rest =~ s/\)[ \t]*$//) {
+				if ($rest =~ s/\)[ \t]*$//) { # (2) in line scope
 					--$thisLineParens;
 				}
 
 				my $content = $rest."\n";
 				$content =~ s/^[ \t]+//;
-				if ($rest =~ /^[ \t]*\<(.*$)/) {
+				
+
+				if ($rest =~ /^[ \t]*\<(.*$)/) { # (3) in line scope
 					my $existingTag = $1;
 					$existingTag = canonicalTagName($existingTag);
 					$content = $tags->{"$existingTag"}->{"content"};
 					defined($content) or die "$0: No content for $existingTag\n";
+				} else {
+					$content =~ s/^[ \t]*\\//; # (4) in line scope
+					$content =~ s/\\([ \t]*)$/$1/; # (5) in line scope
 				}
 
 				if ($thisLineParens < 0) {
@@ -110,17 +125,20 @@ sub readTags
 			my $closeScope = 0;
 
 			$content =~ s/^[ \t]+//;
-			if ($content =~ s/\)[ \t]*$//) {
+			if ($content =~ s/\)[ \t]*$//) { # (2) in block scope
 				$closeScope = 1;
 			}
 
-			if ($content =~ /^[ \t]*\<(.*$)/) {
+			if ($content =~ /^[ \t]*\<(.*$)/) { # (3) in block scope
 				my $existingTag = $1;
 				$existingTag = canonicalTagName($existingTag);
 				my $ptr = $tags->{"$existingTag"};
 				(ref($ptr) eq "HASH") or die "$0: Tag $existingTag not hash ref but ".ref($ptr)."\n";
 				$content = $ptr->{"content"};
 				defined($content) or die "$0: No content for $existingTag\n";
+			} else {
+				$content =~ s/^[ \t]*\\//; # (4) in block scope
+				$content =~ s/\\([ \t]*)$/$1/; # (5) in block scope
 			}
 
 			$multilineContent .= $content;
