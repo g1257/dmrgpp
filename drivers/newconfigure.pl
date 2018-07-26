18eb0b3525b5ef2b5cb49737db1672c89df8469f
Gonzalo Alvarez <gz1@ornl.gov>
2018-07-26 09:46:13 -0400

    configure.pl uses PsiTag

--- drivers/newconfigure.pl
+++ drivers/newconfigure.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 =pod
-Copyright (c) 2009-2017, UT-Battelle, LLC
+Copyright (c) 2009-2018, UT-Battelle, LLC
 All rights reserved
 
-[PsimagLite, Version 1.]
+[DMRG++, Version 5.]
 
 *********************************************************
 THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
@@ -19,43 +19,5 @@ Please see full open source license included in file LICENSE.
 use warnings;
 use strict;
 
-use Getopt::Long qw(:config no_ignore_case);
-use lib "../../PsimagLite/scripts";
-use NewMake;
-use PsiTag;
-
-my $flavor = NewMake::noFlavor();
-my $usage = "USAGE: $0 [-f flavor]\n";
-my $config;
-
-GetOptions('f=s' => \$flavor,
-           'c=s' => \$config) or die "$usage\n";
-
-my @configFiles = ("../../dmrgpp/TestSuite/inputs/ConfigBase.psiTag");
-push @configFiles, $config if (defined($config));
-
-createMakefile(\@configFiles, $flavor);
-
-sub createMakefile
-{
-	my ($configFiles, $flavor) = @_;
-
-	my @drivers = qw(integrator sparseSolverTest testCRSMatrix combineContinuedFraction
-	continuedFractionCollection range kernelPolynomial
-	linearPrediction options randomTest svd testLapack threads loadImbalance testIsClass
-	testMemResolv1 sumDecomposition calculator closuresTest base64test checkRunId
-	testLanczos nested testIoNg);
-
-	my %args;
-	$args{"code"} = "PsimagLite/drivers";
-	$args{"configFiles"} = $configFiles;
-	$args{"flavor"} = $flavor;
-
-	NewMake::backupMakefile();
-	my $fh;
-	open($fh, ">", "Makefile") or die "Cannot open Makefile for writing: $!\n";
-
-	NewMake::main($fh, \%args, \@drivers);
-}
-
+die "$0: Please use configure.pl instead of this program\n";
 
--- lib/newconfigure.pl
+++ lib/newconfigure.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 =pod
-Copyright (c) 2009-2015, UT-Battelle, LLC
+Copyright (c) 2009-2018, UT-Battelle, LLC
 All rights reserved
 
-[PsimagLite, Version 1.0]
+[DMRG++, Version 5.]
 
 *********************************************************
 THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
@@ -19,92 +19,5 @@ Please see full open source license included in file LICENSE.
 use warnings;
 use strict;
 
-use Getopt::Long qw(:config no_ignore_case);
-use lib "../../PsimagLite/scripts";
-use NewMake;
-use PsiTag;
-
-my @drivers = ();
-
-my ($flavor, $config) = ("production", "");
-my $usage = "USAGE: $0 [-f flavor] [-c config]\n";
-
-GetOptions('f=s' => \$flavor,
-           'c=s' => \$config) or die "$usage\n";
-
-my @configFiles = ("../TestSuite/inputs/ConfigBase.psiTag");
-push @configFiles, $config if ($config ne "");
-
-createMakefile(\@configFiles, $flavor);
-
-sub createMakefile
-{
-	my ($configFiles, $flavor) = @_;
-	NewMake::backupMakefile();
-
-	my $fh;
-	open($fh, ">", "Makefile") or die "Cannot open Makefile for writing: $!\n";
-
-	local *FH = $fh;
-	my @units = ("MersenneTwister","Matrix","Mpi","Concurrency",
-	"ProgressIndicator","MemResolv","PsimagLite","PsiBase64",
-	"SpecialFunctions", "Io/TypeToH5");
-	my $combinedUnits = combine("",\@units,".o ");
-	my $combinedUnitsModif = $combinedUnits;
-	$combinedUnitsModif =~ s/Io\///;
-	my $combinedUnits2 = combine("../src/",\@units,".cpp ");
-	my $configContent = NewMake::getConfigContent($configFiles, $flavor);
-
-	print FH<<EOF;
-# DO NOT EDIT!!! Changes will be lost. Use the PsiTag system to configure instead
-# This Makefile was written by $0
-# PsimagLite/lib
-
-$configContent
-
-all: libpsimaglite.a
-libpsimaglite.a: Makefile $combinedUnits AinurSpirit.o AinurState.o
-\tar rcs libpsimaglite.a $combinedUnitsModif AinurSpirit.o AinurState.o
-EOF
-
-	foreach my $unit (@units) {
-		print FH<<EOF;
-$unit.o: ../src/$unit.cpp ../src/$unit.h Makefile
-\t\$(CXX) \$(CPPFLAGS) -c ../src/$unit.cpp
-EOF
-	}
-
-print FH<<EOF;
-AinurSpirit.o: ../src/Ainur/AinurSpirit.cpp ../src/Ainur/AinurSpirit.h Makefile ../src/Ainur/AinurSpirit1.cpp
-\t\$(CXX) \$(CPPFLAGS) -c ../src/Ainur/AinurSpirit.cpp
-
-AinurState.o: ../src/Ainur/AinurState.cpp ../src/Ainur/AinurState.h Makefile 
-\t\$(CXX) \$(CPPFLAGS) -c ../src/Ainur/AinurState.cpp
-
-Makefile.dep: $combinedUnits2 ../src/Ainur/AinurSpirit.cpp ../src/Ainur/AinurState.cpp
-\t\$(CXX) \$(CPPFLAGS) -MM  $combinedUnits2  > Makefile.dep
-
-clean: Makefile.dep
-\trm -f core* *.o *.dep *.a
-
-include Makefile.dep
-
-EOF
-
-	close($fh);
-	print STDERR "File Makefile has been written\n";
-}
-
-sub combine
-{
-	my ($pre,$a,$post) = @_;
-	my $n = scalar(@$a);
-	my $buffer = "";
-	for (my $i = 0; $i < $n; ++$i) {
-		$buffer .= $pre.$a->[$i].$post;
-	}
-
-	return $buffer;
-}
-
+die "$0: Please use configure.pl instead of this program\n";
 
