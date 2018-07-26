902f1b7627f96984f1e03a5cd75284f7fea4f1c4
Gonzalo Alvarez <gz1@ornl.gov>
2018-07-26 09:45:20 -0400

    configure.pl uses PsiTag

--- src/KronUtil/configure.pl
+++ src/KronUtil/configure.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 =pod
-Copyright (c) 2009-2015, UT-Battelle, LLC
+Copyright (c) 2009-2018, UT-Battelle, LLC
 All rights reserved
 
-[DMRG++, Version 4.0]
+[DMRG++, Version 5.]
 
 *********************************************************
 THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
@@ -20,9 +20,12 @@ use warnings;
 use strict;
 
 use lib "../../../PsimagLite/scripts";
-use Make;
+use NewMake;
+use PsiTag;
 
-my ($gccdash) = @ARGV;
+my ($cfiles, $flavor, $gccdash) = @ARGV;
+defined($cfiles) or $cfiles = "../../../dmrgpp/TestSuite/inputs/ConfigBase.psiTag";
+defined($flavor) or $flavor = NewMake::noFlavor();
 defined($gccdash) or $gccdash = "";
 
 my @names = ("KronUtil", "util", "utilComplex", "csc_nnz");
@@ -37,12 +40,12 @@ for (my $i = 0; $i < $total; ++$i) {
 	$dotos .= " $name.o ";
 }
 
-createMakefile($gccdash);
+createMakefile($cfiles, $flavor, $gccdash);
 
 sub createMakefile
 {
-	my ($gccdash) = @_;
-	Make::backupMakefile();
+	my ($cfiles, $flavor, $gccdash) = @_;
+	NewMake::backupMakefile();
 
 	my $fh;
 	open($fh, ">", "Makefile") or die "Cannot open Makefile for writing: $!\n";
@@ -51,24 +54,39 @@ sub createMakefile
 	$args{"code"} = "KronUtil";
 	$args{"additional3"} = "libkronutil.a test1 test2";
 	$args{"path"} = "../";
-	Make::newMake($fh,\@drivers,\%args);
+	$args{"configFiles"} = getConfigFiles($cfiles);
+	$args{"flavor"} = $flavor;
+	NewMake::main($fh, \%args, \@drivers);
 
 	local *FH = $fh;
 print FH<<EOF;
 
-libkronutil.a: $dotos ../Config.make Makefile
+libkronutil.a: $dotos Makefile
 	${gccdash}ar rc libkronutil.a $dotos
 	${gccdash}ranlib libkronutil.a
 
-test1: libkronutil.a test1.o $dotos ../Config.make
+test1: libkronutil.a test1.o $dotos
 	\$(CXX) \$(CFLAGS) -o test1 test1.o libkronutil.a \$(LDFLAGS)
 
-test2: libkronutil.a test2.o ../Config.make
+test2: libkronutil.a test2.o
 	\$(CXX) \$(CFLAGS) -o test2 test2.o libkronutil.a \$(LDFLAGS)
 
 EOF
 
 	close($fh);
 	print STDERR "$0: File Makefile has been written\n";
+}
+
+sub getConfigFiles
+{
+	my ($list) = @_;
+	my @temp = split/ /, $list;
+	my $n = scalar(@temp);
+	for (my $i = 0; $i < $n; ++$i) {
+		next if ($temp[$i] =~ /^\//);
+		$temp[$i] = "../$temp[$i]";
+	}
+
+	return \@temp;
 }
 
