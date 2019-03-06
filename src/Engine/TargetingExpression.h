7d94a8227b7846af8e6b236660b6b27aabf4b1cb
Gonzalo Alvarez <gz1@ornl.gov>
2019-03-06 15:54:28 -0500

    TargetingExpression.h: only to test pvector expressions

---
+++ src/Engine/TargetingExpression.h
@@ -0,0 +1,222 @@
+/*
+Copyright (c) 2009-2013-2019, UT-Battelle, LLC
+All rights reserved
+
+[DMRG++, Version 5.]
+[by G.A., Oak Ridge National Laboratory]
+
+UT Battelle Open Source Software License 11242008
+
+OPEN SOURCE LICENSE
+
+Subject to the conditions of this License, each
+contributor to this software hereby grants, free of
+charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), a
+perpetual, worldwide, non-exclusive, no-charge,
+royalty-free, irrevocable copyright license to use, copy,
+modify, merge, publish, distribute, and/or sublicense
+copies of the Software.
+
+1. Redistributions of Software must retain the above
+copyright and license notices, this list of conditions,
+and the following disclaimer.  Changes or modifications
+to, or derivative works of, the Software should be noted
+with comments and the contributor and organization's
+name.
+
+2. Neither the names of UT-Battelle, LLC or the
+Department of Energy nor the names of the Software
+contributors may be used to endorse or promote products
+derived from this software without specific prior written
+permission of UT-Battelle.
+
+3. The software and the end-user documentation included
+with the redistribution, with or without modification,
+must include the following acknowledgment:
+
+"This product includes software produced by UT-Battelle,
+LLC under Contract No. DE-AC05-00OR22725  with the
+Department of Energy."
+
+*********************************************************
+DISCLAIMER
+
+THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER, CONTRIBUTORS, UNITED STATES GOVERNMENT,
+OR THE UNITED STATES DEPARTMENT OF ENERGY BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
+STATES DEPARTMENT OF ENERGY, NOR THE COPYRIGHT OWNER, NOR
+ANY OF THEIR EMPLOYEES, REPRESENTS THAT THE USE OF ANY
+INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+
+*********************************************************
+
+*/
+
+/** \ingroup DMRG */
+/*@{*/
+
+/*! \file TargetingExpression.h
+ * TBW FIXME TODO
+ */
+
+#ifndef TARGETING_EXPRESSION_H
+#define TARGETING_EXPRESSION_H
+#include <iostream>
+#include "TargetingBase.h"
+#include <stdexcept>
+#include "Pvector.h"
+#include "SpecForTargetingExpression.h"
+#include "CanonicalExpression.h"
+
+namespace Dmrg {
+
+template<typename LanczosSolverType_, typename VectorWithOffsetType_>
+class TargetingExpression : public TargetingBase<LanczosSolverType_,VectorWithOffsetType_> {
+
+	typedef TargetingBase<LanczosSolverType_,VectorWithOffsetType_> BaseType;
+	typedef typename BaseType::TargetingCommonType TargetingCommonType;
+	typedef typename BaseType::WaveFunctionTransfType WaveFunctionTransfType;
+	typedef typename BaseType::ModelType ModelType;
+	typedef typename ModelType::RealType RealType;
+	typedef typename ModelType::InputValidatorType InputValidatorType;
+	typedef typename ModelType::ModelHelperType ModelHelperType;
+	typedef typename ModelHelperType::LeftRightSuperType LeftRightSuperType;
+	typedef typename LeftRightSuperType::BasisWithOperatorsType BasisWithOperatorsType;
+	typedef typename BasisWithOperatorsType::BasisType BasisType;
+	typedef typename BasisType::BlockType BlockType;
+	typedef typename BasisType::QnType QnType;
+	typedef typename TargetingCommonType::StageEnumType StageEnumType;
+	typedef Pvector<VectorWithOffsetType_> PvectorType;
+	typedef typename PsimagLite::Vector<PvectorType*>::Type VectorPvectorType;
+	typedef SpecForTargetingExpression<VectorWithOffsetType_, ModelType>
+	SpecForTargetingExpressionType;
+	typedef typename SpecForTargetingExpressionType::AlgebraType AlgebraType;
+	typedef PsimagLite::CanonicalExpression<SpecForTargetingExpressionType>
+	CanonicalExpressionType;
+	typedef AuxForTargetingExpression<VectorWithOffsetType_, ModelType>
+	AuxForTargetingExpressionType;
+
+public:
+
+	TargetingExpression(const LeftRightSuperType& lrs,
+	                    const ModelType& model,
+	                    const WaveFunctionTransfType& wft,
+	                    const QnType&,
+	                    InputValidatorType& io)
+	    : BaseType(lrs,model,wft,0),
+	      progress_("TargetingExpression")
+	{
+		pvectorsFromInput(io);
+	}
+
+	~TargetingExpression()
+	{
+		for (SizeType i = 0; i < pVectors_.size(); ++i) {
+			delete pVectors_[i];
+			pVectors_[i] = 0;
+		}
+	}
+
+	SizeType sites() const { return 0; }
+
+	SizeType targets() const { return pVectors_.size(); }
+
+	RealType normSquared(SizeType i) const
+	{
+		return PsimagLite::real(this->common().aoe().targetVectors()[i]*
+		                        this->common().aoe().targetVectors()[i]);
+	}
+
+	RealType weight(SizeType i) const
+	{
+		assert(this->common().aoe().noStageIs(StageEnumType::DISABLED));
+		assert(i < pVectors_.size());
+		return pVectors_[i]->weight();
+	}
+
+	RealType gsWeight() const
+	{
+		return 1;
+	}
+
+	void evolve(RealType,
+	            ProgramGlobals::DirectionEnum direction,
+	            const BlockType&,
+	            const BlockType&,
+	            SizeType)
+	{
+		if (direction == ProgramGlobals::INFINITE) return;
+
+		this->common().setAllStagesTo(StageEnumType::WFT_NOADVANCE);
+		computePvectors();
+	}
+
+	void read(typename TargetingCommonType::IoInputType& io,
+	          PsimagLite::String prefix)
+	{
+		this->common().read(io, prefix);
+	}
+
+	void write(const typename PsimagLite::Vector<SizeType>::Type& block,
+	           PsimagLite::IoSelector::Out& io,
+	           PsimagLite::String prefix) const
+	{
+		this->common().write(io, block, prefix);
+	}
+
+private:
+
+	void pvectorsFromInput(InputValidatorType& io)
+	{
+		SizeType total = 0;
+		io.readline(total, "Pvectors=");
+		pVectors_.resize(total);
+		PsimagLite::String tmp;
+		for (SizeType i = 0; i < total; ++i) {
+			io.readline(tmp, "P" + ttos(i) + "=");
+			pVectors_[i] = new PvectorType(tmp);
+		}
+	}
+
+	void computePvectors()
+	{
+		CanonicalExpressionType canonicalExpression(opSpec_);
+		SizeType total = pVectors_.size();
+		AuxForTargetingExpressionType aux(this->model(),
+		                                  this->lrs(),
+		                                  this->common().aoe().psi(),
+		                                  this->common().aoe().targetVectors());
+		const AlgebraType opEmpty(aux);
+		for (SizeType i = 0; i < total; ++i) {
+			AlgebraType tmp(aux);
+			canonicalExpression(tmp, pVectors_[i]->toString(), opEmpty, aux);
+			tmp.finalize();
+			this->common().aoe().targetVectors(i) = tmp.toVectorWithOffset();
+		}
+	}
+
+	PsimagLite::ProgressIndicator progress_;
+	VectorPvectorType pVectors_;
+	SpecForTargetingExpressionType opSpec_;
+};     //class TargetingExpression
+} // namespace Dmrg
+/*@}*/
+#endif
+
