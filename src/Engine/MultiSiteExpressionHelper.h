190e7a6e62a1b0018bb01de462105c3d8772a458
Gonzalo Alvarez <gz1@ornl.gov>
2019-03-25 20:22:20 -0400

    MultiSiteExpressionHelper.h added

---
+++ src/Engine/MultiSiteExpressionHelper.h
@@ -0,0 +1,46 @@
+#ifndef MULTISITEEXPRESSIONHELPER_H
+#define MULTISITEEXPRESSIONHELPER_H
+#include "DmrgSerializer.h"
+#include "Vector.h"
+
+namespace Dmrg {
+
+template<typename LeftRightSuperType, typename VectorWithOffsetType>
+class MultiSiteExpressionHelper {
+
+public:
+
+	typedef DmrgSerializer<LeftRightSuperType, VectorWithOffsetType> DmrgSerializerType;
+	typedef typename PsimagLite::Vector<DmrgSerializerType const*>::Type VectorDmrgSerializerType;
+	typedef typename PsimagLite::Vector<VectorWithOffsetType>::Type VectorVectorWithOffsetType;
+
+	MultiSiteExpressionHelper(SizeType n) : vds_(n, nullptr) {}
+
+	~MultiSiteExpressionHelper()
+	{
+		const SizeType n = vds_.size();
+		for (SizeType i = 0; i < n; ++i) {
+			delete vds_[i];
+			vds_[i] = nullptr;
+		}
+	}
+
+	void push(DmrgSerializerType const* ds,
+	          const VectorWithOffsetType& psi)
+	{
+		SizeType coo = ds->centerOfOrthogonality();
+		assert(coo > 0);
+		--coo;
+
+		delete vds_[coo];
+		vds_[coo] = ds;
+		vgs_[coo] = psi;
+	}
+
+private:
+
+	VectorDmrgSerializerType vds_;
+	VectorVectorWithOffsetType vgs_;
+};
+}
+#endif // MULTISITEEXPRESSIONHELPER_H
