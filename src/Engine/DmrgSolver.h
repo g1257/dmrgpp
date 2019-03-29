g1257/dmrgpp

commit cc0d9ec5231fa84187204260180b2b125c13fccd

Optional saving in RAM of dmrg serializer objects for every CoO

src/Engine/DmrgSolver.h
@@ -591,25 +591,38 @@ obtain ordered
 	           ProgramGlobals::DirectionEnum direction,
 	           SizeType loopIndex)
 	{
-		int saveOption = parameters_.finiteLoop[loopIndex].saveOption;
+		static SizeType counter = 0;
 
-		if (!(saveOption & 1)) return;
 		if (!saveData_) return;
 
+		int saveOption = parameters_.finiteLoop[loopIndex].saveOption;
+
+		if (!(saveOption & 1) || !(saveOption & 16)) return;
+
+
 		const BlockDiagonalMatrixType& transform = truncate_.transform(direction);
-		DmrgSerializerType ds(fsS,fsE,lrs_,target.gs(),transform, direction);
+		DmrgSerializerType* ds = new DmrgSerializerType(fsS,
+		                                                fsE,
+		                                                lrs_,
+		                                                target.gs(),
+		                                                transform,
+		                                                direction);
+		if (saveOption & 16) {
+			target.multiSitePush(ds);
+			return;
+		}
 
-		typename BasisWithOperatorsType::SaveEnum saveOption2 = (saveOption & 4) ?
-		            BasisWithOperatorsType::SaveEnum::ALL :
-		            BasisWithOperatorsType::SaveEnum::PARTIAL;
+		typename BasisWithOperatorsType::SaveEnum saveOption2 = (saveOption & 4)
+		        ? BasisWithOperatorsType::SaveEnum::ALL
+		        : BasisWithOperatorsType::SaveEnum::PARTIAL;
 		SizeType numberOfSites = model_.geometry().numberOfSites();
-
-		static SizeType counter = 0;
 		PsimagLite::String prefix("Serializer");
-		ds.write(ioOut_, prefix, saveOption2, numberOfSites, counter);
+		ds->write(ioOut_, prefix, saveOption2, numberOfSites, counter);
 		PsimagLite::String prefixForTarget = TargetingType::buildPrefix(ioOut_, counter);
 		target.write(sitesIndices_[stepCurrent_], ioOut_, prefixForTarget);
 		++counter;
+		delete ds;
+		ds = 0;
 	}
 
 	bool finalStep(int stepLength,int stepFinal)

src/Engine/ApplyOperatorExpression.h
@@ -80,6 +80,7 @@ DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
 #include "Io/IoSelector.h"
 #include "TargetParamsBase.h"
 #include "StageEnum.h"
+#include "MultiSiteExpressionHelper.h"
 
 namespace Dmrg {
 
@@ -117,6 +118,9 @@ public:
 	typedef typename TimeVectorsBaseType::PairType PairType;
 	typedef StageEnum StageEnumType;
 	typedef typename PsimagLite::Vector<StageEnum>::Type VectorStageEnumType;
+	typedef MultiSiteExpressionHelper<LeftRightSuperType, VectorWithOffsetType>
+	MultiSiteExpressionHelperType;
+	typedef typename MultiSiteExpressionHelperType::DmrgSerializerType DmrgSerializerType;
 
 	ApplyOperatorExpression(const TargetHelperType& targetHelper,
 	                        SizeType indexNoAdvance)
@@ -127,7 +131,8 @@ public:
 	      indexNoAdvance_(indexNoAdvance),
 	      applyOpLocal_(targetHelper.lrs(), targetHelper.withLegacyBugs()),
 	      targetVectors_(0),
-	      timeVectorsBase_(0)
+	      timeVectorsBase_(0),
+	      multiSiteExprHelper_(targetHelper_.model().geometry().numberOfSites() - 2)
 	{}
 
 	~ApplyOperatorExpression()
@@ -425,6 +430,11 @@ public:
 		}
 	}
 
+	void multiSitePush(DmrgSerializerType const* ds) const
+	{
+		multiSiteExprHelper_.push(ds, psi_);
+	}
+
 private:
 
 	void wftOneVector(VectorWithOffsetType& phiNew,
@@ -635,6 +645,7 @@ private:
 	VectorWithOffsetType psi_;
 	typename PsimagLite::Vector<VectorWithOffsetType>::Type targetVectors_;
 	TimeVectorsBaseType* timeVectorsBase_;
+	mutable MultiSiteExpressionHelperType multiSiteExprHelper_;
 };
 
 } // namespace Dmrg

src/Engine/DmrgSerializer.h
@@ -212,6 +212,14 @@ public:
 		m.toSparse(ret);
 	}
 
+	SizeType centerOfOrthogonality() const
+	{
+		SizeType max = lrs_.left().block().size();
+		assert(max > 0);
+		const SizeType site = lrs_.left().block()[max - 1];
+		return (direction_ == ProgramGlobals::DirectionEnum::EXPAND_SYSTEM) ? site : site + 1;
+	}
+
 private:
 
 	void fillOffsets(VectorSizeType& v, const BasisType& basis) const

src/Engine/TargetingBase.h
@@ -121,6 +121,7 @@ public:
 	typedef typename TargetingCommonType::ApplyOperatorExpressionType ApplyOperatorExpressionType;
 	typedef typename PsimagLite::Vector<OperatorType>::Type VectorOperatorType;
 	typedef typename ApplyOperatorExpressionType::StageEnumType StageEnumType;
+	typedef typename ApplyOperatorExpressionType::DmrgSerializerType DmrgSerializerType;
 
 	TargetingBase(const LeftRightSuperType& lrs,
 	              const ModelType& model,
@@ -249,6 +250,11 @@ public:
 
 		io.createGroup(prefix);
 		return prefix;
+	}
+
+	void multiSitePush(DmrgSerializerType const* ds) const
+	{
+		commonTargeting_.aoe().multiSitePush(ds);
 	}
 
 protected:

commit cc430980e95310227b858edd7a59ad57f502ef38

fixes bug introduced in cc0d9ec5231fa84187204260180b2b125c13fccd

src/Engine/DmrgSolver.h
@@ -597,7 +597,7 @@ obtain ordered
 
 		int saveOption = parameters_.finiteLoop[loopIndex].saveOption;
 
-		if (!(saveOption & 1) || !(saveOption & 16)) return;
+		if (!(saveOption & 1) && !(saveOption & 16)) return;
 
 
 		const BlockDiagonalMatrixType& transform = truncate_.transform(direction);

src/Engine/ScaledHamiltonian.h
@@ -32,7 +32,7 @@ public:
 
 		PsimagLite::ProgressIndicator progress("InternalMatrix");
 		PsimagLite::OstringStream msg;
-		msg<<"H'="<<c_<<"*H "<<d_;
+		msg<<"H'="<<c_<<"*H "<<d_<<"      (rank="<<matx_.rows()<<")";
 		progress.printline(msg, std::cout);
 	}
 

TestSuite/inputs/input3501.inp
@@ -26,12 +26,12 @@ GsWeight=0.1
 TSPTau=1.0
 TSPTimeSteps=3
 TSPAlgorithm=Chebyshev
-ChebyshevEpsilon=0.025
-ChebyshevWstar=20
 TSPAdvanceEach=12
 TSPSites 1 3
 TSPLoops 1 0
 TSPProductOrSum=sum
+
+ChebyshevTransform 2 0.1 0
 
 TSPOperator=expression
 OperatorExpression=c