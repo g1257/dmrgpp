70df37aa7687e2f1f40ed09108a24a7f9783ff35
Gonzalo Alvarez <gz1@ornl.gov>
2019-04-23 15:37:40 -0400

    KrylovHelper: cleaned

--- src/Engine/KrylovHelper.h
+++ src/Engine/KrylovHelper.h
@@ -20,33 +20,6 @@ public:
 	KrylovHelper(const SolverParamsType& params)
 	    : params_(params), progress_("KrylovHelper") {}
 
-	//	void calcR(VectorType& r,
-	//	           const MatrixComplexOrRealType& T,
-	//	           const MatrixComplexOrRealType& V,
-	//	           const VectorWithOffsetType& phi,
-	//	           RealType,
-	//	           const VectorRealType& eigs,
-	//	           SizeType timeIndex,
-	//	           SizeType n2,
-	//	           SizeType i0,
-	//	           const TargetParamsType& tstStruct)
-	//	{
-	//		RealType timeDirection = tstStruct.timeDirection();
-
-	//		for (SizeType k=0;k<n2;k++) {
-	//			ComplexOrRealType sum = 0.0;
-	//			for (SizeType kprime=0;kprime<n2;kprime++) {
-	//				ComplexOrRealType tmpV = calcVTimesPhi(kprime,V,phi,i0);
-	//				sum += PsimagLite::conj(T(kprime,k))*tmpV;
-	//			}
-
-	//			RealType tmp = (eigs[k]-E0_)*times_[timeIndex]*timeDirection;
-	//			ComplexOrRealType c = 0.0;
-	//			PsimagLite::expComplexOrReal(c,-tmp);
-	//			r[k] = sum * c;
-	//		}
-	//	}
-
 	void calcR(VectorType& r,
 	           const ActionType& whatRorI,
 	           const MatrixComplexOrRealType& T,
@@ -55,7 +28,8 @@ public:
 	           SizeType n2,
 	           SizeType i0)
 	{
-		bool krylovAbridge = (params_.options.find("KrylovNoAbridge") == PsimagLite::String::npos);
+		bool krylovAbridge = (params_.options.find("KrylovNoAbridge") ==
+		                      PsimagLite::String::npos);
 		SizeType n3 = (krylovAbridge) ? 1 : n2;
 		// ---------------------------------------------------
 		// precompute values of calcVTimesPhi(kprime,v,phi,i0)
