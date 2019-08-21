6c3465b89e32453d2c98c64529e7b6d90f47621f
Gonzalo Alvarez <gz1@ornl.gov>
2019-08-21 18:49:37 -0400

    Parallelizer2: openmp driver

---
+++ src/Parallelizer2OpenMP.h
@@ -0,0 +1,38 @@
+#ifndef PARALLELIZER2OPENMP_H
+#define PARALLELIZER2OPENMP_H
+#include "Vector.h"
+#include <omp.h>
+
+namespace PsimagLite {
+
+template<typename = int>
+class Parallizer2 {
+
+public:
+
+	Parallizer2(SizeType threads) : threads_(threads)
+	{
+		omp_set_num_threads(threads);
+	}
+
+	template<typename SomeLambdaType>
+	void parallelFor(SizeType start, SizeType end, const SomeLambdaType& lambda)
+	{
+#pragma omp parallel for
+		for (SizeType i = start; i < end; ++i)
+			lambda(i, omp_get_thread_num());
+	}
+
+	SizeType numberOfThreads() const
+	{
+		return omp_get_num_threads();
+	}
+
+	String name() const { return "openmp"; }
+
+private:
+
+	SizeType threads_;
+};
+}
+#endif // PARALLELIZER2OPENMP_H
