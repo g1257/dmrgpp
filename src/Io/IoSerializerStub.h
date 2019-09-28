05d48851e88394990f6f52c322ed27a1bfaf4258
Gonzalo Alvarez <gz1@ornl.gov>
2019-09-27 21:45:20 -0400

    IoSerializerEmpty allows PsimagLite to be built without HDF5

--- src/Io/IoSerializerStub.h
+++ src/Io/IoSerializerStub.h
@@ -2,33 +2,23 @@
 #define IOSERIALIZERSTUB_H
 
 #ifndef USE_IO_SIMPLE
+
 #include "IoNgSerializer.h"
 namespace PsimagLite {
+
 typedef IoNgSerializer IoSerializer;
+
 }
+
 #else
-namespace PsimagLite {
-class IoSerializer {
 
-public:
+#include "IoSerializerEmpty.h"
 
-	void createGroup(String root)
-	{
-		std::cerr<<"IoSerializer::createGroup("<<root<<"): I'm just a dummy!\n";
-	}
+namespace  PsimagLite {
 
-	template<typename T>
-	void write(String name, const T& what)
-	{
-		std::cerr<<"IoSerializer::write("<<name<<","<<what<<"): I'm just a dummy!\n";
-	}
+typedef IoSerializerEmpty IoSerializer;
 
-	template<typename T>
-	void read(T& what, String name)
-	{
-		std::cerr<<"IoSerializer::read("<<what<<","<<name<<"): I'm just a dummy!\n";
-	}
-};
 }
+
 #endif
 #endif // IOSERIALIZERSTUB_H
