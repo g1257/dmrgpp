g1257/dmrgpp

commit 86894988832efae71ec8969ba7078fce1a1633c8

Checkpoint no longer depends on Targeting

src/Engine/Checkpoint.h
@@ -90,19 +90,18 @@ DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
 
 namespace Dmrg {
 
-template<typename ParametersType,typename TargetingType_>
+template<typename ModelType, typename WaveFunctionTransfType_>
 class Checkpoint {
 
 public:
 
-	typedef TargetingType_ TargetingType;
-	typedef typename TargetingType::WaveFunctionTransfType WaveFunctionTransfType;
-	typedef typename TargetingType::RealType  RealType;
-	typedef typename TargetingType::BasisWithOperatorsType BasisWithOperatorsType;
+	typedef WaveFunctionTransfType_ WaveFunctionTransfType;
+	typedef typename ModelType::RealType  RealType;
+	typedef typename ModelType::ParametersType ParametersType;
+	typedef typename ModelType::BasisWithOperatorsType BasisWithOperatorsType;
 	typedef typename BasisWithOperatorsType::ComplexOrRealType ComplexOrRealType;
 	typedef typename BasisWithOperatorsType::OperatorsType OperatorsType;
 	typedef typename PsimagLite::IoSelector IoType;
-	typedef typename TargetingType::ModelType ModelType;
 	typedef typename ModelType::InputValidatorType InputValidatorType;
 	typedef typename OperatorsType::OperatorType OperatorType;
 	typedef typename OperatorType::StorageType SparseMatrixType;
@@ -126,8 +125,8 @@ public:
 	                 isObserveCode),
 	    envStack_(systemStack_.onDisk(),
 	              parameters_.filename,
-                  "environ",
-                  isObserveCode),
+	              "environ",
+	              isObserveCode),
 	    progress_("Checkpoint"),
 	    energyFromFile_(0.0),
 	    dummyBwo_("dummy")
@@ -224,19 +223,15 @@ public:
 	// Not related to stacks
 	void read(BasisWithOperatorsType &pS,
 	          BasisWithOperatorsType &pE,
-	          TargetingType& psi,
-	          bool isObserveCode,
-	          PsimagLite::String prefix)
+	          bool isObserveCode)
 	{
-		typename IoType::In ioTmp(parameters_.checkpoint.filename);
+		typename PsimagLite::IoSelector::In ioTmp(parameters_.checkpoint.filename);
 
 		BasisWithOperatorsType pS1(ioTmp, "CHKPOINTSYSTEM", isObserveCode);
-
 		pS = pS1;
+
 		BasisWithOperatorsType pE1(ioTmp, "CHKPOINTENVIRON", isObserveCode);
 		pE = pE1;
-		PsimagLite::IoSelector::In io(parameters_.checkpoint.filename);
-		psi.read(io, prefix);
 	}
 
 	void push(const BasisWithOperatorsType &pS,const BasisWithOperatorsType &pE)
@@ -251,10 +246,10 @@ public:
 		else systemStack_.push(pSorE);
 	}
 
-	const BasisWithOperatorsType& shrink(SizeType what,const TargetingType& target)
+	const BasisWithOperatorsType& shrink(SizeType what)
 	{
-		if (what==ProgramGlobals::ENVIRON) return shrink(envStack_,target);
-		else return shrink(systemStack_,target);
+		if (what==ProgramGlobals::ENVIRON) return shrinkInternal(envStack_);
+		else return shrinkInternal(systemStack_);
 	}
 
 	bool isRestart() const { return isRestart_; }
@@ -394,15 +389,12 @@ private:
 	}
 
 	//! shrink  (we don't really shrink, we just undo the growth)
-	const BasisWithOperatorsType& shrink(DiskOrMemoryStackType& thisStack,
-	                                     const TargetingType& target)
+	const BasisWithOperatorsType& shrinkInternal(DiskOrMemoryStackType& thisStack)
 	{
 		assert(thisStack.size() > 0);
 		thisStack.pop();
 		assert(thisStack.size() > 0);
 		dummyBwo_ =  thisStack.top();
-		// only updates the extreme sites:
-		target.updateOnSiteForCorners(dummyBwo_);
 		return dummyBwo_;
 	}
 

src/Engine/DmrgSolver.h
@@ -127,8 +127,8 @@ public:
 	typedef Truncation<ParametersType,TargetingType> TruncationType;
 	typedef DmrgSerializer<LeftRightSuperType,VectorWithOffsetType> DmrgSerializerType;
 	typedef typename ModelType::GeometryType GeometryType;
-	typedef Checkpoint<ParametersType, TargetingType> CheckpointType;
-	typedef Recovery<ParametersType, CheckpointType> RecoveryType;
+	typedef Checkpoint<ModelType, WaveFunctionTransfType> CheckpointType;
+	typedef Recovery<CheckpointType, TargetingType> RecoveryType;
 	typedef typename DmrgSerializerType::FermionSignType FermionSignType;
 	typedef typename ModelType::ReflectionSymmetryType ReflectionSymmetryType;
 	typedef typename PsimagLite::Vector<BlockType>::Type VectorBlockType;
@@ -235,7 +235,10 @@ public:
 		MyBasisWithOperators pE("BasisWithOperators.Environ");
 
 		if (checkpoint_.isRestart()) {
-			checkpoint_.read(pS, pE, psi, false, "FinalPsi");
+			PsimagLite::IoSelector::In io(parameters_.checkpoint.filename);
+
+			checkpoint_.read(pS, pE, false);
+			psi.read(io, "FinalPsi");
 		} else { // move this block elsewhere:
 
 			RealType time = 0;
@@ -498,6 +501,7 @@ obtain ordered
 		if (stepLength<0)
 			stepLengthCorrected = int((stepLength+sitesPerBlock-1)/sitesPerBlock);
 		int stepFinal = stepCurrent_ + stepLengthCorrected;
+		BasisWithOperatorsType dummyBwo("dummy");
 
 		while (true) {
 
@@ -508,11 +512,14 @@ obtain ordered
 			printerInDetail.print(std::cout, "finite");
 			if (direction == ProgramGlobals::EXPAND_SYSTEM) {
 				lrs_.growLeftBlock(model_, pS, sitesIndices_[stepCurrent_], time);
-				lrs_.right(checkpoint_.shrink(ProgramGlobals::ENVIRON,target));
+				lrs_.right(dummyBwo = checkpoint_.shrink(ProgramGlobals::ENVIRON));
 			} else {
 				lrs_.growRightBlock(model_, pE, sitesIndices_[stepCurrent_], time);
-				lrs_.left(checkpoint_.shrink(ProgramGlobals::SYSTEM,target));
+				lrs_.left(dummyBwo = checkpoint_.shrink(ProgramGlobals::SYSTEM));
 			}
+
+			// only updates the extreme sites:
+			target.updateOnSiteForCorners(dummyBwo);
 
 			lrs_.printSizes("finite",std::cout);
 

src/Engine/Recovery.h
@@ -90,9 +90,10 @@ DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
 
 namespace Dmrg {
 
-template<typename ParametersType,typename CheckpointType>
+template<typename CheckpointType, typename TargetingType>
 class Recovery  {
 
+	typedef typename CheckpointType::ParametersType ParametersType;
 	typedef Recovery<ParametersType,int> RecoveryStaticType;
 	typedef typename CheckpointType::ComplexOrRealType ComplexOrRealType;
 
@@ -125,7 +126,6 @@ public:
 	typedef typename CheckpointType::BasisWithOperatorsType BasisWithOperatorsType;
 	typedef typename CheckpointType::WaveFunctionTransfType WaveFunctionTransfType;
 	typedef typename CheckpointType::IoType IoType;
-	typedef typename CheckpointType::TargetingType TargetingType;
 	typedef typename PsimagLite::Vector<SizeType>::Type VectorSizeType;
 	typedef typename CheckpointType::DiskOrMemoryStackType DiskOrMemoryStackType;
 	typedef typename CheckpointType::DiskStackType DiskStackType;

commit b8d7c94cd470348c0aa86f18dc03f74a2d2624f7

bug fix
Fixes bug introduced in 86894988832efae71ec8969ba7078fce1a1633c8
related to the border sites in restarts

src/Engine/Checkpoint.h
@@ -248,7 +248,7 @@ public:
 		else systemStack_.push(pSorE);
 	}
 
-	const BasisWithOperatorsType& shrink(typename ProgramGlobals::SysOrEnvEnum what)
+	BasisWithOperatorsType& shrink(typename ProgramGlobals::SysOrEnvEnum what)
 	{
 		return (what == ProgramGlobals::SysOrEnvEnum::ENVIRON) ? shrinkInternal(envStack_) :
 		                                                         shrinkInternal(systemStack_);
@@ -392,7 +392,7 @@ private:
 	}
 
 	//! shrink  (we don't really shrink, we just undo the growth)
-	const BasisWithOperatorsType& shrinkInternal(DiskOrMemoryStackType& thisStack)
+	BasisWithOperatorsType& shrinkInternal(DiskOrMemoryStackType& thisStack)
 	{
 		assert(thisStack.size() > 0);
 		thisStack.pop();

src/Engine/DmrgSolver.h
@@ -510,7 +510,6 @@ obtain ordered
 		if (stepLength<0)
 			stepLengthCorrected = int((stepLength+sitesPerBlock-1)/sitesPerBlock);
 		int stepFinal = stepCurrent_ + stepLengthCorrected;
-		BasisWithOperatorsType dummyBwo("dummy");
 
 		VectorRealType energies;
 		while (true) {
@@ -522,14 +521,18 @@ obtain ordered
 			printerInDetail.print(std::cout, "finite");
 			if (direction == ProgramGlobals::DirectionEnum::EXPAND_SYSTEM) {
 				lrs_.growLeftBlock(model_, pS, sitesIndices_[stepCurrent_], time);
-				lrs_.right(dummyBwo = checkpoint_.shrink(ProgramGlobals::SysOrEnvEnum::ENVIRON));
+				BasisWithOperatorsType* dummyBwo =
+				        &checkpoint_.shrink(ProgramGlobals::SysOrEnvEnum::ENVIRON);
+				target.updateOnSiteForCorners(*dummyBwo); // <-- only updates extreme sites
+				lrs_.right(*dummyBwo);
 			} else {
 				lrs_.growRightBlock(model_, pE, sitesIndices_[stepCurrent_], time);
-				lrs_.left(dummyBwo = checkpoint_.shrink(ProgramGlobals::SysOrEnvEnum::SYSTEM));
+				BasisWithOperatorsType* dummyBwo =
+				        &checkpoint_.shrink(ProgramGlobals::SysOrEnvEnum::SYSTEM);
+				target.updateOnSiteForCorners(*dummyBwo); // <-- only updates extreme sites
+				lrs_.left(*dummyBwo);
 			}
 
-			// only updates the extreme sites:
-			target.updateOnSiteForCorners(dummyBwo);
 
 			lrs_.printSizes("finite",std::cout);
 