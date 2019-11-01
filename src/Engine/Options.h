981147f79f7c527513bad0d007f9be5d865251d5
Gonzalo Alvarez <gz1@ornl.gov>
2019-11-01 18:15:35 -0400

    SolverOptions
    SolverOptions=option1,option2,...
    with option1, option2,... now case insensitive.
    PLEASE NOTE: this is the ONLY place
    so far where case doesn't matter.

--- src/Engine/Options.h
+++ src/Engine/Options.h
@@ -1,6 +1,7 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 #include "PsimagLite.h"
+#include <cctype>
 
 namespace Dmrg {
 
@@ -8,6 +9,9 @@ template<typename InputValidatorType>
 class Options {
 
 public:
+
+	typedef typename PsimagLite::String::value_type CharType;
+	typedef typename PsimagLite::String::const_iterator StringConstIterator;
 
 	Options(PsimagLite::String label, InputValidatorType& io)
 	{
@@ -26,11 +30,32 @@ public:
 
 	bool isSet(PsimagLite::String what) const
 	{
-		return (data_.find(what) != PsimagLite::String::npos);
+		return isSubstrCaseInsensitive(data_, what);
 	}
 
 private:
 
+	template<typename charT>
+	struct CaseInsensitiveEqual {
+
+		bool operator()(charT ch1, charT ch2) const
+		{
+			return std::toupper(ch1) == std::toupper(ch2);
+		}
+	};
+
+	bool isSubstrCaseInsensitive(const PsimagLite::String& str1,
+	                             const PsimagLite::String& str2) const
+	{
+		StringConstIterator it = std::search(str1.begin(),
+		                                     str1.end(),
+		                                     str2.begin(),
+		                                     str2.end(),
+		                                     caseInsensitiveEqual_);
+		return (it != str1.end());
+	}
+
+	CaseInsensitiveEqual<CharType> caseInsensitiveEqual_;
 	PsimagLite::String data_;
 };
 }
