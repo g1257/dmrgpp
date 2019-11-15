b3a65b21ab468e0d1970c70a67f95044f6952255
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2019-11-15 11:20:45 -0500

    push version of csr_eye

--- src/KronUtil/csr_eye.cpp
+++ src/KronUtil/csr_eye.cpp
@@ -5,77 +5,32 @@ void csr_eye(const int nrow_B,
              const int ncol_B,
              PsimagLite::CrsMatrix<ComplexOrRealType>& b)
 {
-	/*
+/*
  * ---------------------------------------------------------------------------
  * Out:     sparse identity matrix in compressed sparse row format
  * ---------------------------------------------------------------------------
  */
 
-
-	/*
-  * ----------------------------------------------------
-  * setup boolean array for fast mapping of column index
-  * ----------------------------------------------------
-  */
-
-	int* nnz = new int[nrow_B];
-
-	int ib = 0;
+        const int nnz_B = std::min( nrow_B, ncol_B );
+        b.resize( nrow_B, ncol_B );
+        b.reserve( nnz_B );
 
 
+        int ip = 0;
+        for( int irow=0; irow < nrow_B; irow++) {
+                b.setRow(irow, ip);
+                const int jcol = irow;
+                const bool is_valid = (jcol < ncol_B);
+                if (is_valid) {
+                        b.pushValue( 1 );
+                        b.pushCol( jcol );
+                        ip++;
+                };
+        };
 
-	/*
-   * -------------------------------------------------------
-   * first pass to setup number of nonzeros per row in B
-   * -------------------------------------------------------
-   */
-
-	for(ib=0; ib < nrow_B; ib++) {
-		nnz[ib] = 1;
-	};
-
-
-	/*
-  * ---------------------------------
-  * prefix sum to setup row pointers
-  * ---------------------------------
-  */
-
-	int max_nnz = (nrow_B >= ncol_B) ? nrow_B : ncol_B;
-
-	b.resize(nrow_B, ncol_B, max_nnz);
-	b.setRow(0,0);
-	for(ib=0; ib < nrow_B; ib++)
-		b.setRow(ib+1,b.getRowPtr(ib) + nnz[ib]);
-
-	/*
-   * ------------------------
-   * reset array for 2nd pass
-   * ------------------------
-   */
-	for(ib=0; ib < nrow_B; ib++) {
-		nnz[ib] = 0;
-	};
+        b.setRow(nrow_B, ip);
 
 
-	/*
-   * --------------------------------------
-   * second pass to fill in compress sparse row
-   * data structure
-   * --------------------------------------
-   */
-	for(ib=0; ib < nrow_B; ib++) {
-		int jb = ib;
-		int isvalid =  (0 <= jb) && (jb < ncol_B);
-		if (isvalid) {
-			int ipos = b.getRowPtr(ib) + nnz[ib];
-
-			b.setValues(ipos,1);
-			b.setCol(ipos,jb);
-			nnz[ib] += 1;
-		};
-	};
 
 	b.checkValidity();
-	delete[] nnz;
 }
