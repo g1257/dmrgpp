da9ac8f23d30dd08e328b9f9d0acc3827ce7ccfe
Gonzalo Alvarez <gz1@ornl.gov>
2020-08-03 00:38:05 -0400

    DmrgRunner.h gather DMRG++ code

--- src/ImpuritySolver.h
+++ src/ImpuritySolver.h
@@ -1,26 +1,12 @@
 #ifndef IMPURITYSOLVER_H
 #define IMPURITYSOLVER_H
 
-#include "../../dmrgpp/src/Engine/InputCheck.h"
-#include "../../dmrgpp/src/Engine/Qn.h"
-#include "../../dmrgpp/src/Engine/ProgramGlobals.h"
-#include "../../dmrgpp/src/Engine/SuperGeometry.h"
-#include "../../dmrgpp/src/Engine/ParametersDmrgSolver.h"
-#include "../../dmrgpp/src/Engine/ModelSelector.h"
-#include "../../dmrgpp/src/Engine/ModelHelperLocal.h"
-#include "../../dmrgpp/src/Engine/MatrixVectorKron/MatrixVectorKron.h"
-#include "../../dmrgpp/src/Engine/MatrixVectorOnTheFly.h"
-#include "../../dmrgpp/src/Engine/MatrixVectorStored.h"
-#include "../../dmrgpp/src/Engine/LeftRightSuper.h"
-#include "../../dmrgpp/src/Engine/BasisWithOperators.h"
-#include "../../dmrgpp/src/Engine/DmrgSolver.h"
-#include "../../dmrgpp/src/Engine/VectorWithOffset.h"
-
 #include "PsiBase64.h"
 #include "InputNg.h"
 #include "LanczosSolver.h"
 #include "Vector.h"
 #include "ParamsDmftSolver.h"
+#include "DmrgRunner.h"
 
 namespace Dmft {
 
@@ -32,16 +18,11 @@ public:
 	typedef typename ParamsDmftSolverType::ComplexOrRealType ComplexOrRealType;
 	typedef typename PsimagLite::Real<ComplexOrRealType>::Type RealType;
 	typedef typename PsimagLite::Vector<RealType>::Type VectorRealType;
-	typedef PsimagLite::InputNg<Dmrg::InputCheck> InputNgType;
-	typedef Dmrg::ParametersDmrgSolver<RealType, InputNgType::Readable, Dmrg::Qn>
-	ParametersDmrgSolverType;
-	typedef Dmrg::SuperGeometry<ComplexOrRealType,
-	        InputNgType::Readable,
-	        Dmrg::ProgramGlobals> SuperGeometryType;
-	typedef Dmrg::VectorWithOffset<ComplexOrRealType, Dmrg::Qn> VectorWithOffsetType;
+	typedef DmrgRunner<ComplexOrRealType> DmrgRunnerType;
+	typedef typename DmrgRunnerType::InputNgType InputNgType;
 
 	ImpuritySolver(const ParamsDmftSolverType& params)
-	    : params_(params)
+	    : params_(params), runner_(params_.precision)
 	{}
 
 	// bathParams[0-nBath-1] ==> V ==> hoppings impurity --> bath
@@ -54,7 +35,7 @@ public:
 		PsimagLite::String data2 = modifyBathParams(data, bathParams, params_.echoInput);
 		PsimagLite::String sOptions = "";
 
-		doOneRun(data2, sOptions);
+		runner_.doOneRun(data2, sOptions);
 	}
 
 	ComplexOrRealType gimp(SizeType i)
@@ -105,62 +86,6 @@ private:
 		return buffer;
 	}
 
-	void doOneRun(PsimagLite::String data, PsimagLite::String sOptions)
-	{
-		typedef  PsimagLite::CrsMatrix<std::complex<RealType> > MySparseMatrixComplex;
-		typedef Dmrg::Basis<MySparseMatrixComplex> BasisType;
-		typedef Dmrg::BasisWithOperators<BasisType> BasisWithOperatorsType;
-		typedef Dmrg::LeftRightSuper<BasisWithOperatorsType,BasisType> LeftRightSuperType;
-		typedef Dmrg::ModelHelperLocal<LeftRightSuperType> ModelHelperType;
-		typedef Dmrg::ModelBase<ModelHelperType,
-		        ParametersDmrgSolverType,
-		        InputNgType::Readable,
-		        SuperGeometryType> ModelBaseType;
-
-		Dmrg::InputCheck inputCheck;
-		InputNgType::Writeable ioWriteable(inputCheck, data);
-		InputNgType::Readable io(ioWriteable);
-
-		ParametersDmrgSolverType dmrgSolverParams(io, sOptions, false);
-
-		if (params_.precision > 0) dmrgSolverParams.precision = params_.precision;
-
-		if (dmrgSolverParams.options.isSet("MatrixVectorStored")) {
-			doOneRun2<Dmrg::MatrixVectorStored<ModelBaseType> >(dmrgSolverParams, io);
-		} else if (dmrgSolverParams.options.isSet("MatrixVectorOnTheFly")) {
-			doOneRun2<Dmrg::MatrixVectorOnTheFly<ModelBaseType> >(dmrgSolverParams, io);
-		} else {
-			doOneRun2<Dmrg::MatrixVectorKron<ModelBaseType> >(dmrgSolverParams, io);
-		}
-	}
-
-	template<typename MatrixVectorType>
-	void doOneRun2(const ParametersDmrgSolverType& dmrgSolverParams, InputNgType::Readable& io)
-	{
-		SuperGeometryType geometry(io);
-		if (dmrgSolverParams.options.isSet("printgeometry"))
-			std::cout<<geometry;
-
-		typedef PsimagLite::ParametersForSolver<typename MatrixVectorType::RealType>
-		        ParametersForSolverType;
-		typedef PsimagLite::LanczosSolver<ParametersForSolverType,
-		        MatrixVectorType, typename MatrixVectorType::VectorType> SolverType;
-		typedef typename SolverType::MatrixType::ModelType ModelBaseType;
-
-		//! Setup the Model
-		Dmrg::ModelSelector<ModelBaseType> modelSelector(dmrgSolverParams.model);
-		const ModelBaseType& model = modelSelector(dmrgSolverParams, io, geometry);
-
-		//! Setup the dmrg solver: (vectorwithoffset.h only):
-		typedef Dmrg::DmrgSolver<SolverType, VectorWithOffsetType> DmrgSolverType;
-		DmrgSolverType dmrgSolver(model,io);
-
-		//! Calculate observables:
-		dmrgSolver.main(geometry);
-
-		std::cout.flush();
-	}
-
 	static PsimagLite::String findBathParams(SizeType start,
 	                                         SizeType end,
 	                                         const VectorRealType& bathParams)
@@ -180,6 +105,7 @@ private:
 	}
 
 	const ParamsDmftSolverType& params_;
+	DmrgRunnerType runner_;
 };
 }
 #endif // IMPURITYSOLVER_H
