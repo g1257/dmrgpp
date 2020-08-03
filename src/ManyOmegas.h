d7289d57ec8b1a36d0659c6fd937c759657a00a0
Gonzalo Alvarez <gz1@ornl.gov>
2020-08-03 02:57:55 -0400

    manyOmegas: in-situ measurements

--- src/ManyOmegas.h
+++ src/ManyOmegas.h
@@ -29,12 +29,14 @@ public:
 			io.readline(step, "OmegaStep=");
 			io.readline(total, "OmegaTotal=");
 			io.readline(offset, "OmegaOffset=");
+			io.readline(obs, "Observable=");
 		}
 
 		RealType begin;
 		RealType step;
 		SizeType offset;
 		SizeType total;
+		PsimagLite::String obs;
 	};
 
 	ManyOmegas(PsimagLite::String inputFile,
@@ -54,11 +56,14 @@ public:
 
 	void run(bool dryRun, PsimagLite::String root)
 	{
+		const PsimagLite::String obs = omegaParams_->obs;
+		const PsimagLite::String sOptions = "<gs|" + obs + "P1>,<gs|" +
+		        obs + "|P2>,<gs|" + obs + "|P3>";
+
 		for (SizeType i = omegaParams_->offset; i < omegaParams_->total; ++i) {
 			const RealType omega = i*omegaParams_->step + omegaParams_->begin;
 			PsimagLite::String data2 = modifyOmega(omega);
-			data2 += PsimagLite::String("\nOutputFile=\"") + root + ttos(i) + ";\n";
-			const PsimagLite::String sOptions = "";
+			data2 += PsimagLite::String("\nOutputFile=\"") + root + ttos(i) + "\";\n";
 
 			if (dryRun) {
 				std::cerr<<"ManyOmegas.h:: omega = "<<omega<<" NOT done because -d\n";
