7b573799e200f93c93a9f99fc3fed202a2c37a44
Gonzalo Alvarez <gz1@ornl.gov>
2020-08-18 09:36:41 -0400

    addBathParams instead of modify

--- src/ImpuritySolver.h
+++ src/ImpuritySolver.h
@@ -43,14 +43,14 @@ public:
 	{
 		PsimagLite::String data;
 		InputNgType::Writeable::readFile(data, params_.gsTemplate);
-		PsimagLite::String data2 = modifyBathParams(data, bathParams);
+		PsimagLite::String data2 = addBathParams(data, bathParams);
 		PsimagLite::String insitu = "<gs|nup|gs>";
 
 		runner_.doOneRun(data2, insitu, "-");
 
 		PsimagLite::String data3;
 		InputNgType::Writeable::readFile(data3, params_.omegaTemplate);
-		PsimagLite::String data4 = modifyBathParams(data3, bathParams);
+		PsimagLite::String data4 = addBathParams(data3, bathParams);
 
 		doType(DmrgType::TYPE_0, data4);
 
@@ -102,42 +102,17 @@ public:
 
 private:
 
-	static PsimagLite::String modifyBathParams(PsimagLite::String data,
+	static PsimagLite::String addBathParams(PsimagLite::String data,
 	                                           const VectorRealType& bathParams)
 	{
 		const SizeType nBath = int(bathParams.size() / 2);
-		static const PsimagLite::String label = "dir0:Connectors=";
-		size_t pos1 = data.find(label);
-		if (pos1 == PsimagLite::String::npos)
-			err("modifyBathParams(): cannot find " + label + "\n");
+		const PsimagLite::String connectors = findBathParams(0, nBath, bathParams);
+		const PsimagLite::String label = "dir0:Connectors=[" + connectors + "];\n";
+		const PsimagLite::String potentialV = findBathParams(nBath, 2*nBath, bathParams);
+		const PsimagLite::String label2 = "potentialV=[0, " + potentialV +
+		        ", 0, " + potentialV + "];\n";
 
-		PsimagLite::String connectors = findBathParams(0, nBath, bathParams);
-		PsimagLite::String buffer = data.substr(0, pos1) + label + "[" + connectors + "]";
-
-		size_t pos2 = data.find(";", pos1);
-		if (pos2 < pos1 || pos2 == PsimagLite::String::npos)
-			err("modifyBathParams(): Internal error\n");
-
-		static const PsimagLite::String label2 = "potentialV=";
-		size_t pos3 = data.find(label2);
-		if (pos3 < pos2 + 1 || pos3 == PsimagLite::String::npos)
-			err("modifyBathParams(): cannot find " + label2 + "\n");
-
-		SizeType len = pos3 - pos2 - 1;
-		if (bathParams.size() % 2)
-			err("BathParams not even\n");
-
-		PsimagLite::String potentialV = findBathParams(nBath, 2*nBath, bathParams);
-		buffer += data.substr(pos2, len) + label2 + "[0, " + potentialV +
-		        ", 0, " + potentialV + "]";
-
-		size_t pos4 = data.find(";", pos3);
-		if (pos4 < pos3 + 1 || pos4 == PsimagLite::String::npos)
-			err("modifyBathParams(): internal error(2)\n");
-
-		const SizeType len2 = data.length() - pos4 - 1;
-		buffer += data.substr(pos4, len2);
-		return buffer;
+		return data + label + label2;
 	}
 
 	static PsimagLite::String findBathParams(SizeType start,
