57423a4bf1caafca1441ee07cde420c22d83995e
Gonzalo Alvarez <gz1@ornl.gov>
2020-08-18 07:59:18 -0400

    template inputs added

--- src/Matsubaras.h
+++ src/Matsubaras.h
@@ -13,14 +13,13 @@ public:
 	typedef typename PsimagLite::Vector<RealType>::Type VectorRealType;
 
 	Matsubaras(RealType fictBeta, SizeType nMatsubara)
-	    : fictBeta_(fictBeta), nMatsubara_(nMatsubara)
+	    : fictBeta_(fictBeta), nMatsubara_(nMatsubara), matsubaras_(2*nMatsubara)
 	{
 		for (SizeType i = 0; i < 2*nMatsubara_; ++i) {
 			int n = i - nMatsubara_;
 			matsubaras_[i] = (i > nMatsubara) ? M_PI*n/fictBeta_ : M_PI*(n - 1)/fictBeta_;
 		}
 	}
-
 
 	const RealType& omega(SizeType i) const
 	{
