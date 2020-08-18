bdc41f3fef470b08fe46ecb26b91b6347946475b
Gonzalo Alvarez <gz1@ornl.gov>
2020-08-18 07:30:44 -0400

    Matsubaras.h added (again)

---
+++ src/Matsubaras.h
@@ -0,0 +1,42 @@
+#ifndef MATSUBARAS_H
+#define MATSUBARAS_H
+#include "Vector.h"
+
+namespace Dmft {
+
+template<typename RealType_>
+class Matsubaras {
+
+public:
+
+	typedef RealType_ RealType;
+	typedef typename PsimagLite::Vector<RealType>::Type VectorRealType;
+
+	Matsubaras(RealType fictBeta, SizeType nMatsubara)
+	    : fictBeta_(fictBeta), nMatsubara_(nMatsubara)
+	{
+		for (SizeType i = 0; i < 2*nMatsubara_; ++i) {
+			int n = i - nMatsubara_;
+			matsubaras_[i] = (i > nMatsubara) ? M_PI*n/fictBeta_ : M_PI*(n - 1)/fictBeta_;
+		}
+	}
+
+
+	const RealType& omega(SizeType i) const
+	{
+		assert(i < matsubaras_.size());
+		return matsubaras_[i];
+	}
+
+	SizeType offset() const { return 0; }
+
+	SizeType total() const { return matsubaras_.size(); }
+
+private:
+
+	RealType fictBeta_;         // ficticious beta
+	SizeType nMatsubara_;       // half the number of matsubaras
+	VectorRealType matsubaras_; // wn starting at 0 with the most negative wn
+};
+}
+#endif // MATSUBARAS_H
