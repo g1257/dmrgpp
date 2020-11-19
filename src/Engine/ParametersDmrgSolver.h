db3e1ea5a22da5fa3be73481b0c263b2b0bd0b3a
Gonzalo Alvarez <gz1@ornl.gov>
2020-11-19 03:28:10 -0500

    LoopHelper feature added

--- src/Engine/ParametersDmrgSolver.h
+++ src/Engine/ParametersDmrgSolver.h
@@ -95,6 +95,7 @@ DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
 #include "Options.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include "CanonicalExpression.h"
 
 namespace Dmrg {
 
@@ -133,7 +134,7 @@ See the below for more information and examples on Finite Loops.
 
 \end{itemize}
 */
-template<typename FieldType,typename InputValidatorType, typename QnType>
+template<typename FieldType, typename InputValidatorType, typename QnType>
 struct ParametersDmrgSolver {
 
 	typedef ParametersDmrgSolver<FieldType, InputValidatorType, QnType> ThisType;
@@ -497,8 +498,10 @@ struct ParametersDmrgSolver {
 	                             VectorFiniteLoopType& vfl,
 	                             const MatrixStringType& tmpMat)
 	{
+		SizeType numberOfSites = 0;
+		io.readline(numberOfSites, "TotalNumberOfSites=");
 		for (SizeType i = 0; i < tmpMat.rows(); ++i) {
-			const int length = PsimagLite::atoi(tmpMat(i, 0));
+			const int length = procLength(tmpMat(i, 0), numberOfSites);
 			SizeType m = PsimagLite::atof(tmpMat(i, 1));
 			SizeType bits = PsimagLite::atof(tmpMat(i, 2));
 			FiniteLoop fl(length, m, bits);
@@ -651,6 +654,80 @@ private:
 
 		if (addPidToOutputName) f += "." + ttos(getpid());
 		return f;
+	}
+
+	class LoopLengthSpec {
+
+		class MyContainer {
+
+		public:
+
+			explicit MyContainer(int x) : x_(x) {}
+
+			int value() const { return x_; }
+
+			MyContainer& operator+=(const MyContainer& other)
+			{
+				x_ += other.x_;
+				return *this;
+			}
+
+			MyContainer& operator*=(const MyContainer& other)
+			{
+				x_ *= other.x_;
+				return *this;
+			}
+
+			MyContainer& operator*=(int x)
+			{
+				x_ *= x;
+				return *this;
+			}
+
+			bool operator==(const MyContainer& other) const
+			{
+				return (x_ == other.x_);
+			}
+
+			bool isEmpty() const { return (x_ == 0); }
+
+		private:
+
+			int x_;
+		};
+
+	public:
+
+		typedef MyContainer ResultType;
+		typedef FieldType ComplexOrRealType;
+		typedef int AuxiliaryType;
+
+		static bool isEmpty(ResultType x) { return x.isEmpty(); }
+
+		static bool metaEqual(ResultType x, ResultType y) { return (x == y); }
+
+		ResultType operator()(PsimagLite::String str, int numberOfSites) const
+		{
+			if (str == "%lh") {
+				int nOver2 = numberOfSites/2;
+				if (numberOfSites & 1)
+					err("%lh cannot be used when TotalNumberOfSites is odd\n");
+				return ResultType(nOver2 - 1);
+			}
+
+			int x = PsimagLite::atoi(str);
+			return ResultType(x);
+		}
+	};
+
+	static int procLength(PsimagLite::String val, int numberOfSites)
+	{
+		LoopLengthSpec loopLengthSpec;
+		PsimagLite::CanonicalExpression<LoopLengthSpec> canonicalExpression(loopLengthSpec);
+		typename LoopLengthSpec::ResultType opEmpty(0);
+		typename LoopLengthSpec::ResultType p(1);
+		canonicalExpression(p, val, opEmpty, numberOfSites);
+		return p.value();
 	}
 };
 } // namespace Dmrg
