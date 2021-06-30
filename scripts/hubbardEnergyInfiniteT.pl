aacf54403d4d1f965be352a86152928b5141d2ee
Gonzalo Alvarez <gz1@ornl.gov>
2021-06-29 21:44:15 -0400

    hubbardEnergyInfiniteT.pl: documented

--- scripts/hubbardEnergyInfiniteT.pl
+++ scripts/hubbardEnergyInfiniteT.pl
@@ -10,6 +10,21 @@ defined($m) or die "Usage: $0 sites electronsUp\n";
 my $total = combinatorial($n, $m);
 print "TOTAL $total\n";
 my $sum = 0;
+
+# with contributions from Alberto N.
+# N = number of sites
+# Nup = Ndown = M
+#numberOfDoubleOcc(D, N, M) = comb(N, D) * comb(N - D, M - D) * comb(N - M, M - D)
+# comb(N, D) accounts for the number of ways of having doubly occupied sites on
+# the lattice of N sites. Now, we have M-D ups and downs left to put somewhere.
+#
+#comb(N - D, M - D) accounts for the number of ways of putting the remaining M-D ups
+# on the remaining N-D sites.
+# Now, we need to fill the downs in the N-(M-D)-D=N-M spaces left, so
+#
+# comb(N-M, M - D) accounts for the number of ways of putting the
+# remaining M-D downs on the remaining N-M sites left without ups.
+
 for (my $d = 1; $d <= $m; ++$d) {
 	my $r0 = factorialOf($n - $d);
 	my $r1 = factorialOf($m - $d);
