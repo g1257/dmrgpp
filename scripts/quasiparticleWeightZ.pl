9d5082556ed5a30497d39681e8784133a2c7a27e
Gonzalo Alvarez <gz1@ornl.gov>
2021-09-16 19:50:59 -0400

    quasiparticleWeightZ.pl: mode added
    ./quasiparticleWeightZ.pl file mode
    Mode is optional and defaults to 0
    If 0, then it does Eq.~(4) of Phys. Rev. B 47 14267 (1993)
    else it does the previous calculation.
    
    For mode 0 observe needs to be run with
    ./observe -f input -p 12 "<Q1|c|Q0>"

--- scripts/quasiparticleWeightZ.pl
+++ scripts/quasiparticleWeightZ.pl
@@ -5,17 +5,19 @@ use warnings;
 use utf8;
 use Math::Trig;
 
-my ($file) = @ARGV;
-defined($file) or die "USAGE: $0 filename\n";
+my ($file, $mode) = @ARGV;
+defined($file) or die "USAGE: $0 filename [mode]\n";
+defined($mode) or $mode = "";
 
-my $label1 = "site <Q0|c'|Q1> time";
+my $obs = ($mode eq "") ? "<Q1|c|Q0>" : "<Q0|c'|Q1>";
+my $label1 = "site $obs time";
 
 my @cdaggerI = loadVector($file, $label1);
 my $sites = scalar(@cdaggerI);
 
 my $label2 = "<Q0|c;c'|Q0>";
 
-my @cIcDaggerJ = loadMatrix($file, $label2);
+my @cIcDaggerJ = loadMatrix($file, $label2, $mode);
 
 for (my $mForK = 0; $mForK < $sites; ++$mForK) {
 	doOneMomentumK($mForK, $sites, \@cdaggerI, \@cIcDaggerJ);
@@ -25,24 +27,31 @@ sub doOneMomentumK
 {
 	my ($mForK, $sites, $cdaggerI, $cIcDaggerJ) = @_;
 	my $momentumK = findMomentumK($mForK, $sites);
-	
+
 	my @numerator = ftVector($cdaggerI, $momentumK);
 
-	print STDERR "$0: numerator= @numerator\n";
+	#print STDERR "$0: numerator= @numerator\n";
 
 	my @den = ftMatrix($cIcDaggerJ, $momentumK);
 
-	print STDERR "$0: denominator= @den\n";
+	#print STDERR "$0: denominator= @den\n";
 
 	die "$0: Denominator isn't real\n" if (abs($den[1]) > 1e-5);
 
-	print $mForK." ".$numerator[0]/$den[0]." ".$numerator[1]/$den[0]."\n";
+	print $mForK." ";
+	if ($mode eq "") {
+		$_ = $numerator[0]*$numerator[0] + $numerator[1]*$numerator[1];
+		print "$_\n";
+	} else {
+		print $numerator[0]/$den[0]." ".$numerator[1]/$den[0]."\n";
+	}
 }
 
 sub ftMatrix
 {
 	my ($a, $momentumK) = @_;
 	my $n = scalar(@$a);
+	return (1, 0) if ($n == 0);
 	my $center = $n/2;
 	my ($sumR, $sumI) = (0, 0);
 	for (my $i = 0; $i < $n; ++$i) {
@@ -72,7 +81,7 @@ sub ftVector
 		$sumR += $value*cos($arg);
 		$sumI += $value*sin($arg);
 	}
-	
+
 	return ($sumR, $sumI);
 }
 
@@ -84,7 +93,7 @@ sub loadVector
 	while (<FILE>) {
 		last if (/^\Q$label/);
 	}
-	
+
 	my @a;
 	while (<FILE>) {
 		chomp;
@@ -94,7 +103,7 @@ sub loadVector
 		last unless ($temp[0] =~ /^\d+$/);
 		$a[$temp[0]] = $temp[1];
 	}
-	
+
 	close(FILE);
 	print STDERR "$0: Found ".scalar(@a)." sites for \"$label\" in $file\n";
 	return @a;
@@ -102,17 +111,18 @@ sub loadVector
 
 sub loadMatrix
 {
-	my ($file, $label) = @_;
+	my ($file, $label, $mode) = @_;
+	return () if ($mode eq "");
 	open(FILE, "<", $file) or die "$0: Cannot open $file : $!\n";
 	while (<FILE>) {
 		last if (/^\Q$label/);
 	}
-	
+
 	$_ = <FILE>;
 	chomp;
 	my @sizes = split;
 	die "$0: No matrix dimensions found for $label in $file\n" unless (scalar(@sizes) == 2);
-	
+
 	my @a;
 	my ($rows, $cols) = @sizes;
 	for (my $i = 0; $i < $rows; ++$i) {
@@ -123,7 +133,7 @@ sub loadMatrix
 		(scalar(@temp) == $cols) or die "$0: row $i not with $cols columns\n";
 		$a[$i] = \@temp;
 	}
-	
+
 	close(FILE);
 	print STDERR "$0: Found matrix with ".scalar(@a)." rows for \"$label\" in $file\n";
 	return @a;
