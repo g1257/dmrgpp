08dee16c2144c166ad3ebc50006c4f9b97359050
Gonzalo Alvarez <gz1@ornl.gov>
2021-12-07 22:46:27 -0500

    TargetingExpression: TimeEv.For.Te.h added

---
+++ src/Engine/TimeEvolveForTargetingExpression.h
@@ -0,0 +1,74 @@
+#ifndef TIMEEVOLVEFORTARGETINGEXPRESSION_H
+#define TIMEEVOLVEFORTARGETINGEXPRESSION_H
+#include "Vector.h"
+
+namespace Dmrg {
+
+template<typename PvectorsType>
+class TimeEvolveForTargetingExpression {
+	class OneTimeEvolution {
+
+	public:
+
+		typedef OneTimeEvolution ThisType;
+		typedef typename PsimagLite::Vector<ThisType*>::Type VectorOneTimeEvolutionType;
+		typedef typename PvectorsType::VectorWithOffsetType VectorWithOffsetType;
+		typedef PsimagLite::Vector<SizeType>::Type VectorSizeType;
+
+		OneTimeEvolution(SizeType firstIndex,
+		                 const VectorWithOffsetType& src,
+		                 SizeType timeSteps,
+		                 PvectorsType& pVectors)
+		    : indices(timeSteps)
+		{
+			indices[0] = firstIndex;
+			for (SizeType i = 1; i < timeSteps; ++i) {
+				auto lambda = [this, i](SizeType ind) {
+					this->indices[i] = ind;
+					return "|P" + ttos(ind) + ">";
+				};
+
+				pVectors.createNew(src, lambda);
+			}
+		}
+
+		VectorSizeType indices;
+	};
+
+public:
+
+	typedef OneTimeEvolution OneTimeEvolutionType;
+	typedef typename PsimagLite::Vector<OneTimeEvolution*>::Type VectorOneTimeEvolutionType;
+
+	TimeEvolveForTargetingExpression()
+	{}
+
+	~TimeEvolveForTargetingExpression()
+	{
+		const SizeType n = vEvolutions_.size();
+		for (SizeType i = 0; i < n; ++i) {
+			delete vEvolutions_[i];
+			vEvolutions_[i] = nullptr;
+		}
+	}
+
+	OneTimeEvolution* findThisEvolution(SizeType firstIndex) const
+	{
+		const SizeType n = vEvolutions_.size();
+		for (SizeType i = 0; i < n; ++i)
+			if (vEvolutions_[i]->indices[0] == firstIndex) return vEvolutions_[i];
+
+		return nullptr;
+	}
+
+	void pushBack(OneTimeEvolution* ptr) { vEvolutions_.push_back(ptr); }
+
+	SizeType size() const { return vEvolutions_.size(); }
+
+private:
+
+	VectorOneTimeEvolutionType vEvolutions_;
+
+};
+}
+#endif // TIMEEVOLVEFORTARGETINGEXPRESSION_H
