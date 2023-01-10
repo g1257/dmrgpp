81d4e03ee1299cf2db442fa468ec8ce4266ca967
Gonzalo Alvarez <gz1@ornl.gov>
2023-01-09 20:46:55 -0500

    AinurFromFile: work in progress

--- src/Ainur/AinurMacros.hh
+++ src/Ainur/AinurMacros.hh
@@ -9,15 +9,17 @@ class AinurMacros {
 
 public:
 
+
+
 	struct NativeMacro {
 		std::string type;
 		std::string name;
 		std::string value;
 	};
 
-	AinurMacros()
+	AinurMacros() : AINUR_FROM_FILE("AinurFromFile")
 	{
-		nativeMacros_.push_back({"function", "AinurFromFile", "!AinurFromFile"});
+		nativeMacros_.push_back({"function", AINUR_FROM_FILE, "!" + AINUR_FROM_FILE});
 	}
 
 	SizeType total() const { return nativeMacros_.size(); }
@@ -30,11 +32,26 @@ public:
 
 	std::string procNativeMacro(const std::string& line)
 	{
-		throw RuntimeError("procNativeMacro not implemented\n");
+		if (line.substr(1, AINUR_FROM_FILE.size()) == AINUR_FROM_FILE) {
+			return procAinurFromFile(line);
+		}
+
+		throw RuntimeError("Unknown native macro for " + line + "\n");
 	}
 
 private:
 
+	std::string procAinurFromFile(const std::string& line)
+	{
+		SizeType start = AINUR_FROM_FILE.size() + 1;
+		SizeType len = line.size();
+		assert(len > start);
+		SizeType rest = len - start;
+		std::string content = line.substr(start, rest);
+		throw RuntimeError("unimplemented for " + content + "\n");
+	}
+
+	const std::string AINUR_FROM_FILE;
 	std::vector<NativeMacro> nativeMacros_;
 };
 
