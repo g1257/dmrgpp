a02f45e0ee4d53bb14e398c45471167bd5690713
Gonzalo Alvarez <gz1@ornl.gov>
2023-01-09 19:16:01 -0500

    AinurConvert.cpp and .hh added

---
+++ src/Ainur/AinurConvert.hh
@@ -0,0 +1,120 @@
+#ifndef AINURCONVERT_HH
+#define AINURCONVERT_HH
+#include "../Matrix.h"
+#include "../PsimagLite.h"
+
+namespace PsimagLite {
+
+class AinurConvert {
+
+	template<typename T>
+	struct Action {
+
+		Action(String name, std::vector<T>& t)
+		    : name_(name), t_(t)
+		{}
+
+		template <typename A, typename ContextType>
+		void operator()(A& attr,
+		                ContextType&,
+		                bool&) const;
+
+	private:
+
+		String name_;
+		std::vector<T>& t_;
+	}; // struct Action
+
+	template<typename T>
+	struct ActionMatrix {
+
+		ActionMatrix(String name, Matrix<T>& t)
+		    : name_(name), t_(t)
+		{}
+
+		template <typename A, typename ContextType>
+		void operator()(A& attr,
+		                ContextType&,
+		                bool&) const;
+
+	private:
+
+		String name_;
+		Matrix<T>& t_;
+	}; // struct ActionMatrix
+
+public:
+
+	template<typename T>
+	static void convertInternal(std::vector<T>& t,
+	                            String value,
+	                            typename EnableIf<Loki::TypeTraits<T>::isArith ||
+	                            IsComplexNumber<T>::True ||
+	                            TypesEqual<T, String>::True,
+	                            int>::Type = 0);
+
+	template<typename T>
+	static void convertInternal(Matrix<T>& t,
+	                            String value);
+
+	template<typename T>
+	static void convertInternal(T& t,
+	                            String label,
+	                            typename EnableIf<Loki::TypeTraits<T>::isIntegral,
+	                            int>::Type = 0)
+	{
+		try {
+			t = PsimagLite::atoi(label.c_str());
+		} catch (std::exception& e) {
+			std::cerr<<"FATAL: AinurState: Label " + label + " must be an integer\n";
+			throw e.what();
+		}
+	}
+
+	template<typename T>
+	static void convertInternal(T& t,
+	                            String label,
+	                            typename EnableIf<Loki::TypeTraits<T>::isFloat,
+	                            int>::Type = 0)
+	{
+		try {
+			t = PsimagLite::atof(label.c_str());
+		} catch (...) {
+			err("FATAL: AinurState: Label " + label + " must be a real number\n");
+		}
+	}
+
+	static void convertInternal(String& t, String label)
+	{
+		SizeType l = label.size();
+		if (l > 1 && label[0] == '"' && label[l - 1] == '"') {
+			t = (l == 2) ? "" : label.substr(1,l - 2);
+			return;
+		}
+
+		t = label;
+	}
+
+private:
+
+	static String stringContext(std::string::iterator it,
+	                            std::string::iterator start,
+	                            std::string::iterator end,
+	                            SizeType before = 5,
+	                            SizeType after = 10)
+	{
+		std::string::iterator alpha = it;
+		SizeType counter = 0;
+		while (alpha != start && counter++ < before)
+			--alpha;
+
+		std::string::iterator omega = it;
+		counter = 0;
+		while (omega != end && counter++ < after)
+			++omega;
+
+		return String(alpha, omega);
+	}
+};
+}
+#endif // AINURCONVERT_HH
---
+++ src/Ainur/AinurConvert1.cpp
@@ -0,0 +1,255 @@
+#include "AinurConvert.hh"
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include "AinurDoubleOrFloat.h"
+
+namespace PsimagLite {
+
+struct MyProxyFor {
+
+	static void convert(long unsigned int& t, std::string str)
+	{
+		t = PsimagLite::atoi(str);
+	}
+
+	static void convert(unsigned int& t, std::string str)
+	{
+		t = PsimagLite::atoi(str);
+	}
+
+	static void convert(long int& t, std::string str)
+	{
+		t = PsimagLite::atoi(str);
+	}
+
+	static void convert(int& t, std::string str)
+	{
+		t = PsimagLite::atoi(str);
+	}
+
+	static void convert(double& t, std::string str)
+	{
+		t = PsimagLite::atof(str);
+	}
+
+	static void convert(float& t, std::string str)
+	{
+		t = PsimagLite::atof(str);
+	}
+
+	template<typename T>
+	static void convert(std::complex<T>& t, std::string str)
+	{
+		t = toComplex<T>(str);
+	}
+
+	static void convert(String& t, std::string str)
+	{
+		t = str;
+	}
+
+	template<typename T>
+	static void convert(T& t, std::string str)
+	{
+		String msg("Unknown type ");
+		throw RuntimeError("convert(): " + msg + typeid(t).name() + " for " + str + "\n");
+	}
+
+private:
+
+	template<typename RealType>
+	static std::complex<RealType> toComplex(std::string str)
+	{
+		typedef std::complex<RealType> ComplexType;
+
+		if (str == "i") return ComplexType(0., 1.);
+		if (str == "-i") return ComplexType(0., -1.);
+
+		String buffer;
+		bool flag = false;
+		const SizeType n = str.length();
+		RealType real1 = 0;
+		for (SizeType i = 0; i < n; ++i) {
+			bool isSqrtMinus1 = (str[i] == 'i');
+			if (isSqrtMinus1 && flag)
+				throw RuntimeError("Error parsing number " + str + "\n");
+
+			if (isSqrtMinus1) {
+				flag = true;
+				real1 = atof(buffer.c_str());
+				buffer = "";
+				continue;
+			}
+
+			buffer += str[i];
+		}
+
+		return (flag) ? ComplexType(real1, atof(buffer.c_str())) :
+		                ComplexType(atof(buffer.c_str()), 0);
+	}
+
+};
+
+//---------
+
+boost::spirit::qi::rule<std::string::iterator,
+std::vector<std::string>(),
+boost::spirit::qi::space_type>
+ruleRows()
+{
+	boost::spirit::qi::rule<std::string::iterator,
+	        std::vector<std::string>(),
+	        boost::spirit::qi::space_type> myrule =  "[" >> (+~boost::spirit::qi::char_(",[]"))
+	                                                        % ',' >> "]";
+	return myrule;
+}
+
+//---------
+
+
+//---------
+
+template <typename T>
+template <typename A, typename ContextType>
+void AinurConvert::ActionMatrix<T>::operator()(A& attr,
+                                             ContextType&,
+                                             bool&) const
+{
+	SizeType rows = attr.size();
+	if (rows == 0) return;
+	SizeType cols = attr[0].size();
+	t_.resize(rows, cols);
+	for (SizeType i = 0; i < rows; ++i) {
+		if (attr[i].size() != cols)
+			err("Ainur: Problem reading matrix\n");
+		for (SizeType j = 0; j < cols; ++j)
+			MyProxyFor::convert(t_(i, j), attr[i][j]);
+	}
+}
+
+//---------
+
+template <typename T>
+template <typename A, typename ContextType>
+void AinurConvert::Action<T>::operator()(A& attr,
+                                       ContextType&,
+                                       bool&) const
+{
+	const SizeType n = attr.size();
+	if (n == 2 && attr[1] == "...") {
+		const SizeType m = t_.size();
+		if (m == 0)
+			err("Cannot use ellipsis for vector of unknown size\n");
+		MyProxyFor::convert(t_[0], attr[0]);
+		for (SizeType i = 1; i < m; ++i)
+			t_[i] = t_[0];
+		return;
+	}
+
+	if (n == 2 && attr[1].length() > 4 && attr[1].substr(0, 4) == "...x") {
+		const SizeType m = t_.size();
+		const SizeType l = attr[1].length();
+		const SizeType mm = PsimagLite::atoi(attr[1].substr(4, l - 4));
+		if (m != 0)
+			std::cout<<"Resizing vector to "<<mm<<"\n";
+		t_.resize(mm);
+		MyProxyFor::convert(t_[0], attr[0]);
+		for (SizeType i = 1; i < mm; ++i)
+			t_[i] = t_[0];
+		return;
+	}
+
+	t_.resize(n);
+	for (SizeType i = 0; i < n; ++i)
+		MyProxyFor::convert(t_[i], attr[i]);
+}
+
+//---------
+
+template<typename T>
+void AinurConvert::convertInternal(Matrix<T>& t,
+                                 String value)
+{
+	namespace qi = boost::spirit::qi;
+	typedef std::string::iterator IteratorType;
+	typedef std::vector<std::string> VectorStringType;
+	typedef std::vector<VectorStringType> VectorVectorVectorType;
+
+	IteratorType it = value.begin();
+	qi::rule<IteratorType, VectorStringType(), qi::space_type> ruRows = ruleRows();
+
+	qi::rule<IteratorType, VectorVectorVectorType(), qi::space_type> full =
+	        "[" >> -(ruRows  % ",") >> "]";
+
+	ActionMatrix<T> actionMatrix("matrix", t);
+	bool r = qi::phrase_parse(it,
+	                          value.end(),
+	                          full [actionMatrix],
+	                          qi::space);
+
+	//check if we have a match
+	if (!r) {
+		err("matrix parsing failed near " +
+		    stringContext(it, value.begin(), value.end())
+		    + "\n");
+	}
+
+	if (it != value.end())
+		std::cerr << "matrix parsing: unmatched part exists\n";
+}
+
+template<typename T>
+void AinurConvert::convertInternal(std::vector<T>& t,
+                                 String value,
+                                 typename EnableIf<Loki::TypeTraits<T>::isArith ||
+                                 IsComplexNumber<T>::True ||
+                                 TypesEqual<T, String>::True,
+                                 int>::Type)
+{
+	namespace qi = boost::spirit::qi;
+	typedef std::string::iterator IteratorType;
+	typedef std::vector<std::string> VectorStringType;
+
+	IteratorType it = value.begin();
+	qi::rule<IteratorType, VectorStringType(), qi::space_type> ruRows = ruleRows();
+
+	Action<T> actionRows("rows", t);
+
+	bool r = qi::phrase_parse(it,
+	                          value.end(),
+	                          ruRows [actionRows],
+	                          qi::space);
+
+	//check if we have a match
+	if (!r)
+		err("vector parsing failed near " + stringContext(it, value.begin(), value.end()) + "\n");
+
+	if (it != value.end()) {
+		std::cerr << "vector parsing: unmatched part exists near ";
+		std::cerr << stringContext(it, value.begin(), value.end())<<"\n";
+	}
+}
+
+//---------
+
+template void AinurConvert::convertInternal(Matrix<DoubleOrFloatType>&,String);
+
+template void AinurConvert::convertInternal(Matrix<std::complex<DoubleOrFloatType> >&,
+String);
+
+template void AinurConvert::convertInternal(Matrix<String>&, String);
+
+template void AinurConvert::convertInternal(std::vector<DoubleOrFloatType>&, String, int);
+
+template void AinurConvert::convertInternal(std::vector<std::complex<DoubleOrFloatType> >&,
+String,
+int);
+
+template void AinurConvert::convertInternal(std::vector<SizeType>&, String, int);
+
+template void AinurConvert::convertInternal(std::vector<int>&, String, int);
+
+template void AinurConvert::convertInternal(std::vector<String>&, String, int);
+} // namespace PsimagLite
