g1257/dmrgpp

commit 1754f271b5c73b91fa4256584131a42a2fc5295e

FindSymmetrySector collapses sectors now

src/Engine/ApplyOperatorExpression.h
@@ -129,6 +129,8 @@ public:
 	typedef typename VectorWithOffsetType::VectorType VectorType;
 	typedef typename PsimagLite::Vector<typename
 	PsimagLite::Vector<VectorWithOffsetType*>::Type>::Type VectorVectorVectorWithOffsetType;
+	typedef typename PsimagLite::Vector<VectorType>::Type VectorVectorType;
+	typedef typename PsimagLite::Vector<VectorVectorType>::Type VectorVectorVectorType;
 
 	ApplyOperatorExpression(const TargetHelperType& targetHelper,
 	                        SizeType indexNoAdvance)
@@ -163,7 +165,7 @@ public:
 		}
 	}
 
-	virtual void initPsi(SizeType nsectors, SizeType nexcited)
+	void initPsi(SizeType nsectors, SizeType nexcited)
 	{
 		if (psi_.size() > 0) {
 			bool flag = true;
@@ -318,28 +320,40 @@ public:
 	}
 
 	template<typename SomeBasisType>
-	void setPsi(SizeType sectorIndex,
-	            SizeType excitedIndex,
-	            VectorType& v,
-	            const SomeBasisType& basis,
-	            const VectorSizeType& sectors)
+	void setPsi(VectorVectorVectorType& inV,
+	            const VectorSizeType& sectors,
+	            const SomeBasisType& someBasis,
+	            SizeType nexcited)
 	{
-		if (psi_.size() == 0) {
-			psi_.resize(sectors.size());
-			const SizeType nexcited = targetHelper_.model().params().numberOfExcited;
-			for (SizeType i = 0; i < psi_.size(); ++i)
-				psi_[i].resize(nexcited);
+		const SizeType nsectors = sectors.size();
+
+		assert(nsectors > 0);
+
+		assert(nexcited > 0);
+
+		if (nsectors != inV.size())
+			err("FATAL: inV.size == " + ttos(inV.size()) + " but params.excited.size= "
+			    + ttos(nsectors) + "\n");
+
+		this->initPsi(nsectors, nexcited);
+
+		for (SizeType sectorIndex = 0; sectorIndex < nsectors; ++sectorIndex) {
+			if (inV[sectorIndex].size() != nexcited)
+				err("Expected inV[" + ttos(sectorIndex) + "].size == " +
+				    ttos(nexcited) + " but found " + ttos(inV[sectorIndex].size()) +
+				    " instead\n");
+
+			for (SizeType excitedIndex = 0; excitedIndex < nexcited; ++excitedIndex) {
+
+				if (!psi_[sectorIndex][excitedIndex]) {
+					psi_[sectorIndex][excitedIndex] = new VectorWithOffsetType;
+				}
+
+				psi_[sectorIndex][excitedIndex]->set(inV[sectorIndex][excitedIndex],
+				                                     sectorIndex,
+				                                     someBasis);
+			}
 		}
-
-		assert(sectorIndex < sectors.size());
-		const SizeType sector = sectors[sectorIndex];
-
-		assert(sectorIndex < psi_.size());
-		assert(excitedIndex < psi_[sectorIndex].size());
-
-		if (!psi_[sectorIndex][excitedIndex])
-			psi_[sectorIndex][excitedIndex] = new VectorWithOffsetType;
-		psi_[sectorIndex][excitedIndex]->set(v, sector, basis);
 	}
 
 	void writePsi(PsimagLite::IoSelector::Out& io, PsimagLite::String prefix) const
@@ -863,7 +877,7 @@ private:
 			const SizeType nexcited = psi_[i].size();
 			for (SizeType j = 0; j < nexcited; ++j) {
 				delete psi_[i][j];
-				psi_[i][j] = 0;
+				psi_[i][j] = nullptr;
 			}
 		}
 	}

src/Engine/DensityMatrixSvd.h
@@ -458,6 +458,7 @@ public:
 
 				for (SizeType excitedIndex = 0; excitedIndex < nexcited; ++excitedIndex) {
 					effectiveTargets.push_back(psi[sectorIndex][excitedIndex]);
+					assert(psi[sectorIndex][excitedIndex]);
 					pushOneTarget(*(psi[sectorIndex][excitedIndex]), x++, dir1, dir2);
 					++psiTargets;
 				}

src/Engine/Diagonalization.h
@@ -189,7 +189,6 @@ private:
 	{
 		const SizeType total = lrs.super().partition() - 1;
 		SizeType sum = 0;
-		const bool findSymmetrySector = (parameters_.findSymmetrySector != "");
 
 		for (SizeType j = 0; j < quantumSector_.size(); ++j) {
 			for (SizeType i = 0; i < total; ++i) {
@@ -203,7 +202,7 @@ private:
 
 				const SizeType bs = lrs.super().partition(i + 1) - lrs.super().partition(i);
 
-				if (findSymmetrySector) {
+				if (!parameters_.findSymmetrySector.empty()) {
 					if (!passSymmetryConstraints(qn, lrs.super().block().size(), bs))
 						continue;
 				} else {
@@ -374,6 +373,24 @@ private:
 
 		calcGsEnergy(sectors, energySaved, vecSaved, lrs);
 
+		if (!parameters_.findSymmetrySector.empty()) {
+			constexpr SizeType excitedIndex = 0;
+			SizeType lowestSectorIndex = findLowestSectorIndex(energySaved, vecSaved);
+			assert(lowestSectorIndex < sectors.size());
+			assert(0 < sectors.size());
+
+			sectors[0] = sectors[lowestSectorIndex];
+			sectors.resize(1);
+
+			assert(energySaved.size() > 0);
+			energySaved[0][excitedIndex] = energySaved[lowestSectorIndex][excitedIndex];
+			energySaved.resize(1);
+
+			assert(vecSaved.size() > 0);
+			vecSaved[0][excitedIndex] = vecSaved[lowestSectorIndex][excitedIndex];
+			vecSaved.resize(1);
+		}
+
 		target.set(vecSaved, sectors, lrs.super());
 		energies = energySaved;
 	}
@@ -449,6 +466,43 @@ private:
 
 			vecSaved[j].resize(numberOfExcited);
 		}
+	}
+
+	SizeType findLowestSectorIndex(const VectorVectorRealType& energySaved,
+	                               const VectorVectorVectorType& vecSaved) const
+	{
+		constexpr SizeType excitedIndex =  0;
+
+		SizeType totalSectors = energySaved.size();
+		assert(totalSectors == vecSaved.size());
+		if (totalSectors == 0) {
+			err("FindSymmetrySector requires sectors > 0\n");
+		}
+
+		assert(energySaved[0].size() > 0);
+		SizeType lowestSectorIndex = 0;
+		RealType lowestEnergy = energySaved[lowestSectorIndex][excitedIndex];
+		for (SizeType j = 1; j < totalSectors; ++j) {
+			SizeType nexcited = energySaved[j].size();
+			assert(vecSaved[j].size() == nexcited);
+			if (nexcited > 1) {
+				err("FindSymmetrySector only for nexcited==1\n");
+			}
+
+			if (nexcited == 0) {
+				std::string warnStr = "FindSymmetrySector with nexcited=0\n";
+				std::cerr<<warnStr;
+				std::cout<<warnStr;
+				continue;
+			}
+
+			if (lowestEnergy > energySaved[j][excitedIndex]) {
+				lowestEnergy = energySaved[j][excitedIndex];
+				lowestSectorIndex = j;
+			}
+		}
+
+		return lowestSectorIndex;
 	}
 
 	/** Diagonalise the i-th block of the matrix, return its eigenvectors

src/Engine/TargetingBase.h
@@ -194,32 +194,10 @@ public:
 	                 const VectorSizeType& sectors,
 	                 const BasisType& someBasis)
 	{
-		const SizeType nsectors = sectors.size();
-		const SizeType nexcited = model_.params().numberOfExcited;
-
-		assert(nsectors > 0);
-
-		assert(nexcited > 0);
-
-		if (nsectors != inV.size())
-			err("FATAL: inV.size == " + ttos(inV.size()) + " but params.excited.size= "
-			    + ttos(nsectors) + "\n");
-
-		for (SizeType sectorIndex = 0; sectorIndex < nsectors; ++sectorIndex) {
-			if (inV[sectorIndex].size() != nexcited)
-				err("Expected inV[" + ttos(sectorIndex) + "].size == " +
-				    ttos(nexcited) + " but found " + ttos(inV[sectorIndex].size()) +
-				    " instead\n");
-
-			for (SizeType excitedIndex = 0; excitedIndex < nexcited; ++excitedIndex) {
-
-				commonTargeting_.aoeNonConst().setPsi(sectorIndex,
-				                                      excitedIndex,
-				                                      inV[sectorIndex][excitedIndex],
-				                                      someBasis,
-				                                      sectors);
-			}
-		}
+		commonTargeting_.aoeNonConst().setPsi(inV,
+		                                      sectors,
+		                                      someBasis,
+		                                      model_.params().numberOfExcited);
 	}
 
 	virtual void updateOnSiteForCorners(BasisWithOperatorsType& basisWithOps) const

commit 439556ac1748238c85ecdd3e86b970c0b9cef977

Fixes: 1754f271b5c73b91fa4256584131a42a2fc5295e

src/Engine/ApplyOperatorExpression.h
@@ -349,8 +349,9 @@ public:
 					psi_[sectorIndex][excitedIndex] = new VectorWithOffsetType;
 				}
 
+				assert(sectorIndex < sectors.size());
 				psi_[sectorIndex][excitedIndex]->set(inV[sectorIndex][excitedIndex],
-				                                     sectorIndex,
+				                                     sectors[sectorIndex],
 				                                     someBasis);
 			}
 		}