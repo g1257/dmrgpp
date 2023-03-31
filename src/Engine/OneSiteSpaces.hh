6f2b497416709ecf3c737ab975b9b1968dd16764
Gonzalo Alvarez <gz1@ornl.gov>
2023-03-30 23:02:10 -0400

    SDHS: WFT: OneSiteSpaces added

---
+++ src/Engine/OneSiteSpaces.hh
@@ -0,0 +1,40 @@
+#ifndef ONESITESPACES_HH
+#define ONESITESPACES_HH
+#include "ProgramGlobals.h"
+
+namespace Dmrg {
+
+template<typename ModelType>
+class OneSiteSpaces {
+
+public:
+
+	OneSiteSpaces(SizeType site, ProgramGlobals::DirectionEnum dir, const ModelType& model)
+	    : dir_(dir)
+	{
+		int siteAux = (dir == ProgramGlobals::DirectionEnum::EXPAND_SYSTEM ||
+		               dir == ProgramGlobals::DirectionEnum::INFINITE) ? site + 1
+		                                                               : site - 1;
+		assert(siteAux >= 0);
+		mainHilbert_ = model.hilbertSize(site);
+		auxHilbert_ = model.hilbertSize(siteAux);
+		zeroHilbert_ = model.hilbertSize(0);
+	}
+
+	SizeType hilbertMain() const { return mainHilbert_; }
+
+	SizeType hilbertAux() const { return auxHilbert_; }
+
+	SizeType hilbertZero() const { return zeroHilbert_; }
+
+	ProgramGlobals::DirectionEnum direction() const { return dir_; }
+
+private:
+
+	ProgramGlobals::DirectionEnum dir_;
+	SizeType mainHilbert_;
+	SizeType auxHilbert_;
+	SizeType zeroHilbert_;
+};
+}
+#endif // ONESITESPACES_HH
