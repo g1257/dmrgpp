8a66004e39c18bb0c2a6018de7ac70d2d8de9712
Gonzalo Alvarez <gz1@ornl.gov>
2026-01-16 19:28:53 -0500

    testArnoldiSaI is now catch2

--- PsimagLite/src/tests/testArnoldiSaI.cpp
+++ PsimagLite/src/tests/testArnoldiSaI.cpp
@@ -4,6 +4,9 @@
 #include "PsimagLite.h"
 #include "Random48.h"
 #include <cassert>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 using ComplexType          = std::complex<double>;
 using ComplexOrRealType    = ComplexType;
@@ -76,7 +79,7 @@ void printEigenvector(std::ostream&                                os,
 
 /* This example does Arnoldi shift-and-invert for a (small) matrix in RAM
  * using PsimagLite's ArnoldiSaI solver */
-void test_arnoldi_sai()
+TEST_CASE("Full Arnoldi shift-and-invert of a random matrix", "[ArnoldiSaI]")
 {
 	/* We fill a dense matrix */
 	int                                   n   = 64;
@@ -90,25 +93,26 @@ void test_arnoldi_sai()
 		for (int j = 0; j < n; ++j) {
 			m(i, j) = rng.random() * max + min;
 		}
-
-		// m(i, i) = 0;
 	}
 
-	// Diag the matrix for testing (FIXME: move to a function)
+	// Diag the matrix for testing
 	PsimagLite::Matrix<ComplexOrRealType> m_copy(m); // m_copy will be overwritten by geev
 	std::vector<ComplexType>              eigenvalues(n);
 	PsimagLite::Matrix<ComplexOrRealType> vl_unused(n, n);
 	PsimagLite::Matrix<ComplexOrRealType> right_eigenvectors(n, n);
 	geev('N', 'V', m_copy, eigenvalues, vl_unused, right_eigenvectors);
 
+	REQUIRE(n == eigenvalues.size());
+	REQUIRE_THAT(0., Catch::Matchers::WithinAbs(std::abs(std::imag(eigenvalues[0])), 1e-5));
+
 	// Matrix eigenvalues are these
-	std::cout << "Actual eigs\n";
-	std::cout << eigenvalues << "\n\n";
+	// std::cout << "Actual eigs\n";
+	// std::cout << eigenvalues << "\n\n";
 
 	unsigned int the_one = findTheOne(eigenvalues);
 	assert(the_one < eigenvalues.size());
-	std::cout << "Lowest eig of L = " << the_one << " " << eigenvalues[the_one] << "\n";
-	printEigenvector(std::cout, right_eigenvectors, the_one);
+	// std::cout << "Lowest eig of L = " << the_one << " " << eigenvalues[the_one] << "\n";
+	// printEigenvector(std::cout, right_eigenvectors, the_one);
 
 	// Now we do Arnoldi Shift-and-Invert
 	double sigma = 0.1; // small value here
@@ -126,17 +130,15 @@ void test_arnoldi_sai()
 	PsimagLite::Matrix<ComplexOrRealType> matrix_inverse(m_copy);
 	std::vector<ComplexType>              inverse_eigs(n);
 	geev('N', 'V', m_copy, inverse_eigs, vl_unused, right_eigenvectors);
-	std::cout << "Inverse eigs\n";
-	std::cout << inverse_eigs << "\n\n";
 
 	// Predict for comparison the eigenvalues of the sai
-	std::vector<ComplexType> predicted_eigs(n);
-	for (int i = 0; i < n; ++i) {
-		predicted_eigs[i] = 1.0 / (eigenvalues[i] - sigma);
-	}
-
-	std::cout << "Predicted eigs\n";
-	std::cout << predicted_eigs << "\n\n";
+	ComplexType predicted_eig = 1.0 / (eigenvalues[0] - sigma);
+	int         index_inverse = n - 1;
+	REQUIRE(index_inverse < inverse_eigs.size());
+	ComplexType value_inverse = inverse_eigs[index_inverse];
+	CHECK(std::real(predicted_eig) == Catch::Approx(std::real(value_inverse)));
+	CHECK_THAT(std::abs(std::imag(predicted_eig)),
+	           Catch::Matchers::WithinAbs(std::abs(std::imag(value_inverse)), 1e-5));
 
 	// Finally do ArnoldiSaI
 	/* We convert the dense matrix into sparse
@@ -169,13 +171,5 @@ void test_arnoldi_sai()
 	constexpr SizeType             bogus_index = 0;
 	arnoldi_sai.computeOneState(eigenvalue, eigenvector, initial, bogus_index);
 	std::cout << "Eigenvalue = " << eigenvalue << "\n";
-
-	std::cout << "\nEstimated eigenvector \n";
-	std::cout << eigenvector << "\n";
-}
-
-int main(int argc, char* argv[])
-{
-	PsimagLite::Concurrency concurrency(&argc, &argv, /*nthreads=*/1);
-	test_arnoldi_sai();
+	CHECK(std::real(eigenvalue) == Catch::Approx(std::real(eigenvalues[the_one])));
 }
