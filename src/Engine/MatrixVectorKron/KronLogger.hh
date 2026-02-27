daf979dbf16b9ac9c701dc4e3d9d86098b98bd04
Gonzalo Alvarez <gz1@ornl.gov>
2026-02-27 00:52:41 -0500

    kronlogger prints matrices

--- src/Engine/MatrixVectorKron/KronLogger.hh
+++ src/Engine/MatrixVectorKron/KronLogger.hh
@@ -14,7 +14,9 @@ namespace Dmrg {
 template <typename ModelType> class KronLogger {
 public:
 
-	using InitKronType = InitKronHamiltonian<ModelType>;
+	using InitKronType            = InitKronHamiltonian<ModelType>;
+	using ArrayOfMatStructType    = typename InitKronType::ArrayOfMatStructType;
+	using MatrixDenseOrSparseType = typename ArrayOfMatStructType::MatrixDenseOrSparseType;
 
 	KronLogger(const InitKronType& init_kron, const std::string& filename)
 	    : progress_("KronLogger")
@@ -56,7 +58,11 @@ public:
 		if (!fout_)
 			return;
 
+		SizeType nC      = init_kron_.connections();
+		SizeType total   = init_kron_.numberOfPatches(InitKronType::OLD);
+		SizeType offsetX = init_kron_.offsetForPatches(InitKronType::NEW, outPatch);
 		*fout_ << "outPatch=" << outPatch << "\n";
+		*fout_ << "nC=" << nC << " total=" << total << " offsetX=" << offsetX << "\n";
 	}
 
 	void two(SizeType inPatch)
@@ -64,18 +70,68 @@ public:
 		if (!fout_)
 			return;
 
-		*fout_ << "->inPatch=" << inPatch << "\n";
+		SizeType offsetY = init_kron_.offsetForPatches(InitKronType::OLD, inPatch);
+		*fout_ << separationLevel(1) << "inPatch=" << inPatch << "\n";
+		*fout_ << separationLevel(1) << "offsetY=" << offsetY << "\n";
 	}
 
-	void three(SizeType ic)
+	void three(SizeType outPatch, SizeType inPatch, SizeType ic)
 	{
 		if (!fout_)
 			return;
 
-		*fout_ << "-->ic=" << ic << "\n";
+		const ArrayOfMatStructType& xiStruct = init_kron_.xc(ic);
+		const ArrayOfMatStructType& yiStruct = init_kron_.yc(ic);
+
+		const bool performTranspose = (init_kron_.useLowerPart() && (outPatch < inPatch));
+
+		const MatrixDenseOrSparseType* Amat
+		    = performTranspose ? xiStruct(inPatch, outPatch) : xiStruct(outPatch, inPatch);
+
+		const MatrixDenseOrSparseType* Bmat
+		    = performTranspose ? yiStruct(inPatch, outPatch) : yiStruct(outPatch, inPatch);
+
+		*fout_ << separationLevel(2) << "ic=" << ic << "\n";
+		*fout_ << separationLevel(2) << "performTranspose=" << performTranspose << "\n";
+
+		// TODO: Convert printing to Matrix Market Format
+		assert(Amat);
+		*fout_ << separationLevel(2)
+		       << "Matrix A follows in format: " + matrixFormat(Amat->isDense()) + "\n";
+		printMatrixDenseOrSparse(*Amat);
+
+		assert(Bmat);
+		*fout_ << separationLevel(2)
+		       << "Matrix B follows in format: " + matrixFormat(Bmat->isDense()) + "\n";
+		printMatrixDenseOrSparse(*Bmat);
 	}
 
 private:
+
+	static std::string separationLevel(SizeType n)
+	{
+		std::string tmp;
+		for (SizeType i = 0; i < n; ++i) {
+			tmp += " ";
+		}
+
+		return tmp;
+	}
+
+	static std::string matrixFormat(bool b) { return (b) ? "dense" : "sparse"; }
+
+	void printMatrixDenseOrSparse(const MatrixDenseOrSparseType& mat)
+	{
+		if (!fout_) {
+			err("printMatrixDenseOrSparse: InternalError: KronLogger not enabled!?\n");
+		}
+
+		if (mat.isDense()) {
+			*fout_ << mat.dense();
+		} else {
+			*fout_ << mat.sparse();
+		}
+	}
 
 	PsimagLite::ProgressIndicator progress_;
 	std::ofstream*                fout_;
