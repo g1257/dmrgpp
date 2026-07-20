9818a77c50ec9f60a46b5df95bb8cbca12e3207f
Daniel Arndt <arndtd@ornl.gov>
2026-07-20 13:08:08 -0400

    Fix

--- dmrg/KronUtil/csr_kron_mult.cpp
+++ dmrg/KronUtil/csr_kron_mult.cpp
@@ -278,11 +278,13 @@ void csr_kron_mult_method(const int  imethod,
 		 * ---------------------------------------------
 		 */
 
+#if 0
     std::cerr << "nrow_A: " << nrow_A << ' '
               << "nnz_A: " << csr_nnz(a) << ' '
               << "nrow_B: " << nrow_B << ' '
-              << "nnz_B: " << csr_nnz(b) << '\n';
-            
+              << "nnz_B: " << csr_nnz(b) << '\n';     
+#endif
+       
 		int ia = 0;
 		int ka = 0;
 		int ib = 0;
