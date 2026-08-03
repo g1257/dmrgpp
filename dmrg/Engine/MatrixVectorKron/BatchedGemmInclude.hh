08f367ffcf0c8b627e96f2ba6a49da4dec86d832
Daniel Arndt <arndtd@ornl.gov>
2026-08-03 11:43:13 -0400

    restore BatchedGemmInclude.hh

--- dmrg/Engine/MatrixVectorKron/BatchedGemmInclude.hh
+++ dmrg/Engine/MatrixVectorKron/BatchedGemmInclude.hh
@@ -1,15 +1,12 @@
 #ifndef BATCHEDGEMMINCLUDE_HH
 #define BATCHEDGEMMINCLUDE_HH
 #include "DMRGConfig.h"
-#if 0 // KOKKOS_BATCHED
-#include "BatchedGemmKokkos.h"
-#define BATCHED_GEMM ::Dmrg::BatchedGemmKokkos
-#elif defined(PLUGIN_SC)
+#ifdef PLUGIN_SC
 #include "BatchedGemmPluginSc.h"
-#define BATCHED_GEMM ::Dmrg::BatchedGemmPluginSc
+#define BATCHED_GEMM BatchedGemmPluginSc
 #else
 #include "BatchedGemmCpu.h"
-#define BATCHED_GEMM ::Dmrg::BatchedGemmCpu
+#define BATCHED_GEMM BatchedGemmCpu
 #endif
 #include <PsimagLite/PsimagLite.h>
 
@@ -21,17 +18,15 @@ public:
 
 	static void failIfNotSupported()
 	{
-#if defined(KOKKOS_BATCHED) || defined(PLUGIN_SC)
+#ifdef PLUGIN_SC
 		return;
 #endif
-		err("BatchedGemm needs DMRG_BUILD_BATCHED_KOKKOS=ON or -DPLUGIN_SC\n");
+		err("BatchedGemm needs -DPLUGIN_SC in Config.make\n");
 	}
 
 	static std::string info()
 	{
-#if 1 // KOKKOS_BATCHED
-		return "KokkosKernels";
-#elif defined(PLUGIN_SC)
+#ifdef PLUGIN_SC
 		return "PLUGIN_SC";
 #else
 		return "";
