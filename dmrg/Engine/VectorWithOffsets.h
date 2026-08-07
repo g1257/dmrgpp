c175729579ce43a5b5105f297079ba382bafaf4d
Gonzalo Alvarez <gz1@ornl.gov>
2026-08-06 23:02:07 -0400

    vectorwithoffsets code path marked from input

--- dmrg/Engine/VectorWithOffsets.h
+++ dmrg/Engine/VectorWithOffsets.h
@@ -159,10 +159,26 @@ public:
 	 * This overload exists for interface compatibility but always reports a fatal error.
 	 */
 	template <typename SomeBasisType>
-	VectorWithOffsets(SizeType, SizeType, const SomeBasisType&)
-	    : progress_("VectorWithOffset")
+	VectorWithOffsets(SizeType weight, SizeType sector, const SomeBasisType& someBasis)
+	    : progress_("VectorWithOffsets")
+	    , size_(someBasis.size())
+	    , index2Sector_(size_)
+	    , data_(someBasis.partition() - 1)
+	    , offsets_(someBasis.partition())
 	{
-		err("VectorWithOffsets::ctor() FATAL: wrong execution path!\n");
+		for (SizeType i = 0; i < offsets_.size(); ++i)
+			offsets_[i] = someBasis.partition(i);
+
+		assert(data_.size() < offsets_.size());
+		if (offsets_[data_.size()] != size_)
+			err("VectorWithOffsets::ctor(): FATAL: internal error\n");
+
+		assert(sector < data_.size());
+		data_[sector].resize(weight);
+		QnType qn = someBasis.pseudoQn(sector);
+		nzMsAndQns_.push_back(PairQnType(sector, qn));
+
+		setIndex2Sector();
 	}
 
 	/*!
